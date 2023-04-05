b0bbd43173adee6d49f97a835e98ab3612633835
Mohit <mohitkhatri4272@gmail.com>
2023-04-06 01:26:44 +0530

    client side validation on form

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,22 @@
 import { AppBar, Toolbar } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { HeaderButton } from './styled';
-import Signin from 'components/Signin/Signin';
+import Signin from 'components/Login/Login';
 
 const Header = () => {
-  return (
-    <AppBar position="static">
-      <Toolbar>
-        <HeaderButton color="inherit" component={Link} to="/">
-          Home
-        </HeaderButton>
-        <HeaderButton color="inherit" component={Link} to="/expenses">
-          Expenses
-        </HeaderButton>
-        <Signin/>
-      </Toolbar>
-    </AppBar>
-  );
+   return (
+      <AppBar position='static'>
+         <Toolbar>
+            <HeaderButton color='inherit' component={Link} to='/'>
+               Home
+            </HeaderButton>
+            <HeaderButton color='inherit' component={Link} to='/expenses'>
+               Expenses
+            </HeaderButton>
+            <Signin />
+         </Toolbar>
+      </AppBar>
+   );
 };
 
 export default Header;

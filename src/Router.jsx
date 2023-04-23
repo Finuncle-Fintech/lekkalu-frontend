d72e83fdd378681a730cd7c34e3b7be74609ad72
Ivan Lautaro Garcilazo <ivangarcilazo.dv@gmail.com>
2023-04-23 07:39:00 -0300

    feature/footer-support

--- src/Router.jsx
+++ src/Router.jsx
@@ -4,14 +4,16 @@ import Expenses from 'components/Expenses/Expenses';
 import Header from "components/Header/Header";
 import EmiCalculator from "pages/EmiCalculator";
 import { Context } from 'provider/Provider';
+import SupportPopUp from "components/Support/PopUp/PopUp";
+import Footer from "components/Footer/Footer";
 
 const RouterComponent = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<><Header /><Charts /></>} />
-        <Route path="/expenses" element={<><Header /><Expenses Context={Context} /></>} />
-        <Route path="/loan_emi_calculator" element={<><Header/><EmiCalculator/></> } />
+        <Route path="/" element={<><Header /><Charts /><Footer/><SupportPopUp/></>} />
+        <Route path="/expenses" element={<><Header /><Expenses Context={Context} /><Footer/><SupportPopUp/></>} />
+        <Route path="/loan_emi_calculator" element={<><Header/><EmiCalculator/><Footer/><SupportPopUp/></> } />
       </Routes>
     </Router>
   );

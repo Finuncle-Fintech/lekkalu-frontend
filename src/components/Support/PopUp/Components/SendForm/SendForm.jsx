1219552f3c4819f5bd6de3cb570b51a3f8fe2d4b
Ivan Lautaro Garcilazo <ivangarcilazo.dv@gmail.com>
2023-05-08 21:30:37 -0300

    41-feedback-form-api-call

--- src/components/Support/PopUp/Components/SendForm/SendForm.jsx
+++ src/components/Support/PopUp/Components/SendForm/SendForm.jsx
@@ -3,8 +3,8 @@ import check from './static/4964-check-mark-success-animation.gif'
 export default function SendForm({emailSended}){
     return(
         <div className={styles.container} style={{display: !emailSended&&'none'}} >
-            <img className={styles.image} src={check} alt="" />
-            <p>Email sended succesful</p>
+            <img className={styles.image}  src={check} alt="" />
+            <p>Email sent successfully</p>
         </div>
     )
 }

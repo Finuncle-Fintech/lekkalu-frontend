093e17411496101b0f3eef063302b46fc2274929
bbedwin <bbedwin@bharathbrands.in>
2023-05-14 10:33:50 +0300

    feat: adds signin and signup pages

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,18 +1,82 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Charts from 'components/Charts/Charts';
-import Expenses from 'components/Expenses/Expenses';
+import Charts from "components/Charts/Charts";
+import Expenses from "components/Expenses/Expenses";
 import Header from "components/Header/Header";
-import { Context } from 'provider/Provider';
+import { Context } from "provider/Provider";
+import IncomeStatement from "pages/income-statement/IncomeStatement";
+import EmiCalculator from "pages/EmiCalculator";
+import SupportPopUp from "components/Support/PopUp/PopUp";
+import Footer from "components/Footer/Footer";
+import Signin from "./pages/Signin/Signin";
+import Signup from "./pages/Signup/Signup";
 
 const RouterComponent = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<><Header /><Charts /></>} />
-        <Route path="/expenses" element={<><Header /><Expenses Context={Context} /></>} />
+        <Route
+          path="/"
+          element={
+            <>
+              <Header />
+              <Charts />
+              <Footer />
+              <SupportPopUp Context={Context} />
+            </>
+          }
+        />
+          <Route
+              path="/signin"
+              element={
+                  <>
+                      <Signin />
+                      <SupportPopUp Context={Context} />
+                  </>
+              }
+          />
+          <Route
+              path="/signup"
+              element={
+                  <>
+                      <Signup />
+                      <SupportPopUp Context={Context} />
+                  </>
+              }
+          />
+        <Route
+          path="/expenses"
+          element={
+            <>
+              <Header />
+              <Expenses Context={Context} />
+              <Footer />
+              <SupportPopUp Context={Context} />
+            </>
+          }
+        />
+        <Route
+          path="/loan_emi_calculator"
+          element={
+            <>
+              <Header />
+              <EmiCalculator />
+              <Footer />
+              <SupportPopUp Context={Context} />
+            </>
+          }
+        />
+        <Route
+          path="/income-statement"
+          element={
+            <>
+              <Header />
+              <IncomeStatement Context={Context} />
+            </>
+          }
+        />
       </Routes>
     </Router>
   );
-}
+};
 
 export default RouterComponent;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
-import { AppBar, Toolbar } from '@mui/material';
-import { Link } from 'react-router-dom';
-import { HeaderButton } from './styled';
+import {AppBar, Toolbar, Box} from "@mui/material";
+import { Link } from "react-router-dom";
+import { HeaderButton } from "./styled";
 
 const Header = () => {
   return (
@@ -11,6 +11,28 @@ const Header = () => {
         </HeaderButton>
         <HeaderButton color="inherit" component={Link} to="/expenses">
           Expenses
+        </HeaderButton>
+        <HeaderButton color="inherit" component={Link} to="/income-statement">
+          Income Statement
+        </HeaderButton>
+        <HeaderButton
+          color="inherit"
+          component={Link}
+          to="/loan_emi_calculator"
+        >
+          EMI Calculator
+        </HeaderButton>
+
+        <Box sx={{ flexGrow: 1 }} />
+
+        <HeaderButton color="inherit" component={Link} to="/signin">
+          Sign in
+        </HeaderButton>
+        <HeaderButton color="inherit" component={Link} to="/signup">
+          Sign up
+        </HeaderButton>
+        <HeaderButton color="inherit" component={Link} to="/signout">
+          Sign out
         </HeaderButton>
       </Toolbar>
     </AppBar>

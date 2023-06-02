9bb58b1ca376b4a37b6defb342aedaa36b174b5f
Ivan Lautaro Garcilazo <ivangarcilazo.dv@gmail.com>
2023-06-02 15:38:09 -0300

    feat/rebase-resolve-conflics-and-fix-modal-close

--- src/components/Expenses/components/CircleLoading.jsx
+++ src/components/Expenses/components/CircleLoading.jsx
@@ -1,5 +1,4 @@
 function CircleLoading() {
-<<<<<<< HEAD
     return (
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -31,36 +30,3 @@ function CircleLoading() {
   }
   
   export default CircleLoading;
-=======
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="25"
-      height="25"
-      preserveAspectRatio="xMidYMid"
-      viewBox="0 0 100 100"
-    >
-      <circle
-        cx="50"
-        cy="50"
-        r="35"
-        fill="none"
-        stroke="#FFFFFF"
-        strokeDasharray="164.93361431346415 56.97787143782138"
-        strokeWidth="10"
-      >
-        <animateTransform
-          attributeName="transform"
-          dur="1s"
-          keyTimes="0;1"
-          repeatCount="indefinite"
-          type="rotate"
-          values="0 50 50;360 50 50"
-        ></animateTransform>
-      </circle>
-    </svg>
-  );
-}
-
-export default CircleLoading;
->>>>>>> 7d2e197 (feat/loader-fix)

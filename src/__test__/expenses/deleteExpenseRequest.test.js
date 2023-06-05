02f520345dd0583eb3dbe11154ef7d762a3beaa8
Ivan Lautaro Garcilazo <ivangarcilazo.dv@gmail.com>
2023-06-05 01:28:11 -0300

    fix/testError

--- src/__test__/expenses/deleteExpenseRequest.test.js
+++ src/__test__/expenses/deleteExpenseRequest.test.js
@@ -1,23 +1,29 @@
-import React, { createContext } from "react";
-import { render, fireEvent, waitFor, screen } from "@testing-library/react";
+import React, {createContext} from "react";
+import {render, fireEvent, waitFor, screen} from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Expenses from "components/Expenses/Expenses";
-import { mockState } from "__test__/data/Expenses";
+import {mockState} from "__test__/data/Expenses";
 
 const TestContext = createContext(mockState);
 
 describe("deleteExpenseRequest", () => {
-  test("successfully deletes an expense", async () => {
-    render(
-      <TestContext.Provider value={mockState}>
-        <Expenses Context={TestContext}/>
-      </TestContext.Provider>
-    );
+    test("successfully deletes an expense", async () => {
+        render(
+            <TestContext.Provider value={mockState}>
+                <Expenses Context={TestContext}/>
+            </TestContext.Provider>
+        );
 
-    fireEvent.click(screen.getAllByPlaceholderText("delete-expense")[0]);
+        fireEvent.click(screen.getAllByPlaceholderText("delete-expense")[0]);
+        await waitFor(() => {
+            // expect(mockState.deleteExpenseRequest).toHaveBeenCalled();
+            expect(screen.getByText('Delete')).toBeInTheDocument()
+        });
+        fireEvent.click(screen.getByText('Delete'));
 
-    await waitFor(() => {
-      expect(mockState.deleteExpenseRequest).toHaveBeenCalled();
+        await waitFor(() => {
+            // expect(mockState.deleteExpenseRequest).toHaveBeenCalled();
+            expect(mockState.deleteExpenseRequest).toHaveBeenCalled();
+        });
     });
-  });
 });

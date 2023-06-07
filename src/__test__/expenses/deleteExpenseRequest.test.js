7232c9aee75bdb3a321f02d01233f99c47e6c3c1
Ivan Lautaro Garcilazo <ivangarcilazo.dv@gmail.com>
2023-06-07 04:27:03 -0300

    feat/modalForExcelStatus

--- src/__test__/expenses/deleteExpenseRequest.test.js
+++ src/__test__/expenses/deleteExpenseRequest.test.js
@@ -26,5 +26,4 @@ describe("deleteExpenseRequest", () => {
             expect(mockState.deleteExpenseRequest).toHaveBeenCalled();
         });
     });
-    
 });
--- src/components/Expenses/ExpenseList.jsx
+++ src/components/Expenses/ExpenseList.jsx
@@ -1,57 +1,59 @@
 import React from 'react';
-import { Table, TableBody, TableRow, TableCell, IconButton } from '@mui/material';
+import {Table, TableBody, TableRow, TableCell, IconButton} from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import Swal from 'sweetalert2';
 
-const ExpensesList = ({ expenses, getTagNames, setEditIndex, deleteExpense }) => {
+const ExpensesList = ({expenses, getTagNames, setEditIndex, deleteExpense}) => {
 
-  const deleteHandler = (expense) =>{
+    const deleteHandler = (expense) => {
 
-      Swal.fire({
-        title:'Are you sure?',
-        text:`You won't be able to revert this`,
-        icon:'warning',
-        showConfirmButton:true,
-        showCancelButton:true,
-        confirmButtonText:'Delete',
-        confirmButtonColor:"Red"
-      }).then((res)=>{
-        switch(res.isConfirmed){
-          case true:
-              deleteExpense(expense.id)
-              Swal.fire(
-                'Deleted',
-                'Your expense deleted successfully.',
-                'success'
-              )
-        }
-      })
-    
-  }
-  return (
-    <Table>
-      <TableBody>
-        {expenses &&
-          Boolean(expenses.length) &&
-          expenses.map((expense, index) =>{ return(
-            <TableRow key={expense.id}>
-              <TableCell>{expense.amount} ₹</TableCell>
-              <TableCell>{getTagNames(expense.tags)}</TableCell>
-              <TableCell>{new Date(expense.time).toDateString(0)}</TableCell>
-              <TableCell>
-                <IconButton placeholder="edit-expense" edge="end" onClick={() => setEditIndex(index)}>
-                  <EditIcon />
-                </IconButton>
-                <IconButton placeholder="delete-expense" edge="end" onClick={() =>{deleteHandler(expense)}}>
-                  <DeleteIcon/>
-                </IconButton>
-              </TableCell>
-            </TableRow>
-          )})}
-      </TableBody>
-    </Table>
-  );
+        Swal.fire({
+            title: 'Are you sure?',
+            text: `You won't be able to revert this`,
+            icon: 'warning',
+            showConfirmButton: true,
+            showCancelButton: true,
+            confirmButtonText: 'Delete',
+            confirmButtonColor: "Red"
+        }).then((res) => {
+            if (res.isConfirmed) {
+                deleteExpense(expense.id)
+                Swal.fire(
+                    'Deleted',
+                    'Your expense deleted successfully.',
+                    'success'
+                )
+            }
+        })
+
+    }
+
+    return (
+        <Table>
+            <TableBody>
+                {expenses &&
+                    Boolean(expenses.length) &&
+                    expenses.map((expense, index) => (
+                        <TableRow key={expense.id}>
+                            <TableCell>{expense.amount} ₹</TableCell>
+                            <TableCell>{getTagNames(expense.tags)}</TableCell>
+                            <TableCell>{new Date(expense.time).toDateString(0)}</TableCell>
+                            <TableCell>
+                                <IconButton placeholder="edit-expense" edge="end" onClick={() => setEditIndex(index)}>
+                                    <EditIcon/>
+                                </IconButton>
+                                <IconButton placeholder="delete-expense" edge="end" onClick={() => {
+                                    deleteHandler(expense)
+                                }}>
+                                    <DeleteIcon/>
+                                </IconButton>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+            </TableBody>
+        </Table>
+    );
 };
 
 export default ExpensesList;

cf0fd69abe1c6e55adb57dc56d551c672ecea561
Ivan Lautaro Garcilazo <ivangarcilazo.dv@gmail.com>
2023-06-29 02:57:34 -0300

    feat/TestSignIn-SignUp

--- src/__test__/SignIn/SignIn.test.js
+++ src/__test__/SignIn/SignIn.test.js
@@ -1,8 +1,10 @@
+/* eslint-disable testing-library/no-node-access */
 import React, { createContext, useContext } from "react";
 import Signin from "pages/Signin/Signin";
 import '@testing-library/jest-dom/extend-expect'
-import { fireEvent, render, screen, waitFor } from '@testing-library/react'
+import { fireEvent, prettyDOM, render, screen, waitFor, act } from '@testing-library/react'
 import { BrowserRouter } from "react-router-dom";
+import swal from "sweetalert2";
 
 const mockState = {
   fetchToken: jest.fn()
@@ -16,7 +18,7 @@ jest.mock('react-router-dom', ()=>({
 jest.mock('@mui/material/Typography', () => {
     return {
       __esModule: true,
-      default: () => <></>, // Mock que devuelve un fragmento vacío
+      default: () => <></>, // Mock return fragment empty
     };
 });
 jest.mock('@mui/material/Link',()=>{
@@ -29,7 +31,6 @@ jest.mock('@mui/material/Link',()=>{
 const TestContext = createContext(mockState);
 
 describe('SignIn test funcionality', () => {
-    
    
     test('Successfull button login funcionality when is clicked', async() => {
         const view = render(
@@ -38,7 +39,7 @@ describe('SignIn test funcionality', () => {
                     <Signin Context={TestContext} />
                 </TestContext.Provider>
             </BrowserRouter>
-        );    
+        ); 
     
         const buttonSignIn = screen.getByText('Sign In')
 
@@ -48,4 +49,38 @@ describe('SignIn test funcionality', () => {
             expect(mockState.fetchToken).toHaveBeenCalledTimes(1)
         })
     });
+
+    test('Show error when user doesn\'t exist',async()=>{
+        const view = render(
+            <BrowserRouter>
+                <TestContext.Provider value={mockState}>
+                    <Signin Context={TestContext} />
+                </TestContext.Provider>
+            </BrowserRouter>
+        );    
+
+        //Arrange
+        const errorText = 'User with provided details does not exist'
+        //SpyOn on jest is util for get methods of objects
+        const spySwal = jest.spyOn(swal, 'fire')
+        const buttonSignIn = screen.getByText('Sign In')
+        const form = buttonSignIn.parentNode
+        const usernameInput = form.firstChild.children[1].firstChild
+        const passwordInput = form.children[1].children[1].firstChild
+      
+        //Act
+        fireEvent.change(usernameInput, { target: { value: 'nameUser, this username does not exist' } });
+        fireEvent.change(passwordInput, { target: { value: 'badPasswordThis Passowrd does not exist.' } });
+        fireEvent.click(buttonSignIn);
+
+        //Assert
+        await waitFor(()=>{
+            expect(spySwal).toBeCalledTimes(1)
+        })
+        await waitFor(()=>{
+            expect(swal.getHtmlContainer().textContent).toEqual(errorText)
+        })
+
+    })
+
 });

d2796b213310e108ac21c9b435e5cbd58ceffff0
Ivan Lautaro Garcilazo <ivangarcilazo.dv@gmail.com>
2023-07-13 12:23:58 -0300

    fix/resetCommitsAndPull

--- src/components/Charts/AssetsDepreciationsChart.jsx
+++ src/components/Charts/AssetsDepreciationsChart.jsx
@@ -27,7 +27,7 @@ export default function AssetsdepreciationChart({data}){
                     const months = [
                         'January', 'February', 'March', 'April', 'May', 'June',
                         'July', 'August', 'September', 'October', 'November', 'December'
-                      ];
+                      ]
 
                     const year = dateNum.slice(2,4)
                     const month = dateNum.slice(5,7)

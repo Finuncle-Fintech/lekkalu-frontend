72f69834a4ae6712c83a8e484954249905c2f6ad
Edwin Karimi <edumuriithi58@gmail.com>
2023-09-09 07:32:03 +0300

    feat: add filtering to expenses (#80)
    * feat: add filtering to expenses
    
    * fix: date format error
    
    * fix: filter form
    
    * fix: filter dates cleared on submit

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -4,6 +4,12 @@ import {
   TablePagination,
   IconButton
 } from "@mui/material";
+import dayjs from 'dayjs';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 import { SkipNext, SkipPrevious } from '@mui/icons-material';
 import ExpenseFormModal from "./ExpensesModal";
 import { ModalContainer, modalSuccesCreated } from "./styled";
@@ -19,17 +25,21 @@ const Expenses = ({ Context }) => {
     tags,
     createTag,
     fetchExpenses,
+    filterExpensesByDate,
     deleteExpenseRequest,
     createExpenseRequest,
     changeExpenseRequest,
     fetchTags,
     authToken
   } = useContext(Context);
-
+  const getDate = new Date()
   const [editIndex, setEditIndex] = useState(null);
   const [page, setPage] = useState(0);
-  const [ loadExcelStatus, setLoadExcelStatus ]  = useState(false)
-  const [newData, setNewData ] = useState([])
+  const [loadExcelStatus, setLoadExcelStatus] = useState(false)
+  const [newData, setNewData] = useState([])
+  const [fromDate, setFromDate] = useState(null)
+  const [toDate, setToDate] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
   const rowsPerPage = 10;
 
   useEffect(() => {
@@ -43,8 +53,8 @@ const Expenses = ({ Context }) => {
       .filter((tag) => tag !== undefined);
   };
 
-  const getTagNames =(tagValues) => {
-    const tagNames = tagValues&&tagValues
+  const getTagNames = (tagValues) => {
+    const tagNames = tagValues && tagValues
       .map((tagValue) => {
         const foundTag = tags.find((tag) => tag.id === tagValue);
         return foundTag ? foundTag.name : null;
@@ -59,7 +69,7 @@ const Expenses = ({ Context }) => {
 
     const file = files[0];
     const reader = new FileReader();
-    reader.onload = async(event) => {
+    reader.onload = async (event) => {
       const data = event.target.result;
       const workbook = XLSX.read(data, { type: "binary" });
 
@@ -68,11 +78,11 @@ const Expenses = ({ Context }) => {
 
       const parsedData = XLSX.utils.sheet_to_json(sheet);
 
-      
+
       if (parsedData.length > 0) {
-        setNewData([{excelLength:parsedData.length}])
-        
-        const loadExcel = ()=>{
+        setNewData([{ excelLength: parsedData.length }])
+
+        const loadExcel = () => {
           setLoadExcelStatus(true)
 
           const promise = parsedData.map(async entry => {
@@ -100,10 +110,10 @@ const Expenses = ({ Context }) => {
       }
       setLoadExcelStatus(false)
       Swal.fire({
-        icon:'success',
-        title:'The expense was added correctly.',
-        timer:2300,
-        timerProgressBar:true,
+        icon: 'success',
+        title: 'The expense was added correctly.',
+        timer: 2300,
+        timerProgressBar: true,
       })
     };
 
@@ -123,12 +133,38 @@ const Expenses = ({ Context }) => {
   };
 
   const returnExpenseToEdit = () => {
-    return editIndex !== null ? { ... expenses[editIndex], tags: getTagObjects(expenses[editIndex].tags) } : null
+    return editIndex !== null ? { ...expenses[editIndex], tags: getTagObjects(expenses[editIndex].tags) } : null
   };
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
   };
+
+  const clearFilters = async () => {
+    setIsLoading(true)
+    setFromDate(null)
+    setToDate(null)
+    await fetchExpenses(page, rowsPerPage)
+    setIsLoading(false)
+  }
+
+  const handleFilterSubmit = async (e) => {
+    e.preventDefault()
+    setIsLoading(true)
+
+    if (fromDate === null && toDate === null) {
+      await fetchExpenses(page, rowsPerPage);
+    } else {
+      const from = new Date(fromDate).toLocaleDateString('en-US')
+      const to = new Date(toDate).toLocaleDateString('en-US')
+
+      const filterFromDate = dayjs(from).format('YYYY-MM-DD')
+      const filterToDate = dayjs(to).format('YYYY-MM-DD')
+
+      await filterExpensesByDate(page, rowsPerPage, filterFromDate, filterToDate)
+    }
+    setIsLoading(false)
+  }
 
   return (
     <ModalContainer>
@@ -138,7 +174,7 @@ const Expenses = ({ Context }) => {
         expenseToEdit={returnExpenseToEdit()}
         editIndex={editIndex}
         onCancelEdit={() => setEditIndex(null)}
-        loadExcelStatus = {loadExcelStatus}
+        loadExcelStatus={loadExcelStatus}
         handleFileUpload={handleFileUpload}
         createExpenseExcelStatus = {newData}
         expenses = {expenses}
@@ -146,32 +182,75 @@ const Expenses = ({ Context }) => {
         authToken = {authToken}
       />
       <Typography variant="h6">Expense List</Typography>
-      <div style={{ display: 'flex', alignItems: 'center' }}>
-        <IconButton
-          onClick={() => {
-            setPage((prevPage) => Math.max(prevPage - 3, 0));
-          }}
-        >
-          <SkipPrevious />
-        </IconButton>
-        <TablePagination
-          component="div"
-          count={70}
-          page={page}
-          onPageChange={handleChangePage}
-          rowsPerPage={rowsPerPage}
-          rowsPerPageOptions={[]}
-          labelDisplayedRows={() => ''}
-        />
-        <IconButton
-          onClick={() => {
-            setPage((prevPage) => Math.min(prevPage + 3, 6));
-          }}
-        >
-          <SkipNext />
-        </IconButton>
-        {page * 10 + 1} - {page * 10 + 10} of 70
+      <div className="d-flex justify-content-between align-items-center my-3">
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <IconButton
+            onClick={() => {
+              setPage((prevPage) => Math.max(prevPage - 3, 0));
+            }}
+          >
+            <SkipPrevious />
+          </IconButton>
+          <TablePagination
+            component="div"
+            count={70}
+            page={page}
+            onPageChange={handleChangePage}
+            rowsPerPage={rowsPerPage}
+            rowsPerPageOptions={[]}
+            labelDisplayedRows={() => ''}
+          />
+          <IconButton
+            onClick={() => {
+              setPage((prevPage) => Math.min(prevPage + 3, 6));
+            }}
+          >
+            <SkipNext />
+          </IconButton>
+          {page * 10 + 1} - {page * 10 + 10} of 70
+        </div>
+
+        <Box component="form" onSubmit={handleFilterSubmit} className="d-flex justify-ceontent-between">
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <div>
+              <div components={['DatePicker']} className="d-flex justify-content-center align-items-center">
+                <DatePicker
+                  label="From"
+                  value={fromDate}
+                  maxDate={dayjs(getDate.toLocaleDateString())}
+                  onChange={(newValue) => setFromDate(newValue)}
+                />
+                <div className="mx-2">-</div>
+                <DatePicker
+                  label="To"
+                  value={toDate}
+                  maxDate={dayjs(getDate.toLocaleDateString())}
+                  onChange={(newValue) => setToDate(newValue)}
+                />
+              </div>
+            </div>
+            <Button
+              type="submit"
+              variant="contained"
+              sx={{ py: 2, px: 4, ml: 1 }}
+              disabled={isLoading}
+            >
+              Filter
+            </Button>
+            <Button
+              type="button"
+              variant="contained"
+              sx={{ py: 2, px: 4, ml: 1 }}
+              disabled={isLoading}
+              onClick={clearFilters}
+              className="bg-danger"
+            >
+              Clear
+            </Button>
+          </LocalizationProvider>
+        </Box>
       </div>
+
       <ExpensesList
         expenses={expenses}
         getTagNames={getTagNames}

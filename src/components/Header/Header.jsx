6f6cf409d13356be1dfdaa2e23593ea7eee1d26e
Beteab Admassu <beteab.admassu.tadele@gmail.com>
2023-09-13 15:12:07 +0300

    navBar_issue_fixed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,17 @@
 import React, { useContext, useState } from "react";
-import {Box,CssBaseline,IconButton,List,ListItem,ListItemText,Drawer,Hidden,} from "@mui/material";
+import { Link, useLocation } from "react-router-dom";
+import {
+  Box,
+  CssBaseline,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+  Drawer,
+  Hidden,
+  Typography,
+  Button,
+} from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import Logo from "./HeaderComponents/Logo";
 import BasicMenu from "./HeaderComponents/BasicMenu";
@@ -7,8 +19,8 @@ import { Context } from "provider/Provider";
 
 const styles = {
   appBar: {
-    width: "100%",
-    minHeight: "25vh",
+    maxWidth: "100vw",
+    minHeight: "15vh",
     color: "white",
     backgroundColor: "primary.main",
     display: "flex",
@@ -20,6 +32,7 @@ const styles = {
     color: "black",
     width: "49px",
     height: "51px",
+    margin: "10px",
   },
   responsiveIconButton: {
     backgroundColor: "white",
@@ -27,13 +40,18 @@ const styles = {
     marginRight: "15px",
   },
   menuDrawer: {
-    marginTop: "15vh",
+    backgroundColor: "primary.main",
   },
 };
 
 const Header = () => {
   const { signOut, authToken } = useContext(Context);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const location = useLocation();
+
+  const isActive = (pathname) => {
+    return location.pathname === pathname;
+  };
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -42,7 +60,18 @@ const Header = () => {
   return (
     <Box sx={styles.appBar}>
       <CssBaseline />
-      <Logo />
+      <Box
+        sx={{
+          width: "34vw",
+          height: "30%",
+          display: "flex",
+          justifyContent: "left",
+          alignItems: "inherit",
+          paddingLeft: "5vw",
+        }}
+      >
+        <Logo />
+      </Box>
       <Box
         sx={{
           width: "32vw",
@@ -58,8 +87,15 @@ const Header = () => {
           }}
         >
           {/* List of pages depicted by icons */}
-          <Hidden smDown>
-            <IconButton sx={styles.iconButton}>
+          <Hidden mdDown>
+            <IconButton
+              sx={{
+                ...styles.iconButton,
+                backgroundColor: isActive("/") ? "#0F4C91" : "white",
+              }}
+              component={Link}
+              to={"/"}
+            >
               <img
                 width="25"
                 height="30"
@@ -67,7 +103,15 @@ const Header = () => {
                 alt="home-page"
               />
             </IconButton>
-            <IconButton sx={styles.iconButton}>
+            <IconButton
+              sx={{
+                ...styles.iconButton,
+                backgroundColor: isActive("/balance") ? "#0F4C91" : "white",
+                color: isActive("/balance") ? "white" : "black",
+              }}
+              component={Link}
+              to={"/balance"}
+            >
               <img
                 width="28"
                 height="28"
@@ -75,7 +119,13 @@ const Header = () => {
                 alt="balance-scale-right"
               />
             </IconButton>
-            <IconButton sx={styles.iconButton}>
+            <IconButton
+              sx={{
+                ...styles.iconButton,
+                backgroundColor: isActive("/goal") ? "#0F4C91" : "white",
+                color: isActive("/goal") ? "white" : "black",
+              }}
+            >
               <img
                 width="28"
                 height="28"
@@ -83,7 +133,15 @@ const Header = () => {
                 alt="goal"
               />
             </IconButton>
-            <IconButton sx={styles.iconButton}>
+            <IconButton
+              sx={{
+                ...styles.iconButton,
+                backgroundColor: isActive("/expenses") ? "#0F4C91" : "white",
+                color: isActive("/expenses") ? "white" : "black",
+              }}
+              component={Link}
+              to={"/expenses"}
+            >
               <img
                 width="28"
                 height="28"
@@ -95,7 +153,7 @@ const Header = () => {
         </Box>
       </Box>
 
-      <Hidden smDown>
+      <Hidden mdDown>
         <BasicMenu />
       </Hidden>
 
@@ -110,19 +168,106 @@ const Header = () => {
         anchor="left"
         open={isMenuOpen}
         onClose={toggleMenu}
-        sx={styles.menuDrawer}
+        sx={{
+          "& .MuiDrawer-paper": {
+            backgroundColor: "primary.main",
+            width: "300px",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          },
+        }}
       >
-        <List>
-          <ListItem button onClick={toggleMenu}>
-            <ListItemText primary="Profile" />
-          </ListItem>
-          <ListItem button onClick={toggleMenu}>
-            <ListItemText primary="My account" />
-          </ListItem>
-          <ListItem button onClick={toggleMenu}>
-            <ListItemText primary="Logout" />
-          </ListItem>
-        </List>
+        <Box margin="20%">
+          <Logo />
+        </Box>
+
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            width: "100%",
+          }}
+        >
+          <Button
+            variant="contained"
+            sx={{
+              backgroundColor: isActive("/") ? "#D9D9D9" : "primary.main",
+              color: isActive("/") ? "black" : "white",
+              fontWeight: 1000,
+              margin: "10px",
+            }}
+            component={Link}
+            to={"/"}
+          >
+            <img
+              width="25"
+              height="30"
+              src="https://img.icons8.com/sf-regular-filled/48/1A1A1A/home-page.png"
+              alt="home-page"
+              style={{ marginRight: "10px" }}
+            />
+            Home
+          </Button>
+          <Button
+            variant="contained"
+            sx={{
+              backgroundColor: isActive("/balance") ? "#D9D9D9" : "#1976D2",
+              color: isActive("/balance") ? "black" : "white",
+              fontWeight: 1000,
+              margin: "10px",
+            }}
+            component={Link}
+            to={"/balance"}
+          >
+            <img
+              width="28"
+              height="28"
+              src="https://img.icons8.com/windows/32/1A1A1A/balance-scale-right.png"
+              alt="balance-scale-right"
+              style={{ marginRight: "10px" }}
+            />
+            Balance
+          </Button>
+          <Button
+            variant="contained"
+            sx={{
+              backgroundColor: isActive("/goal") ? "#D9D9D9" : "#1976D2",
+              color: isActive("/goal") ? "black" : "white",
+              fontWeight: 1000,
+              margin: "10px",
+            }}
+          >
+            <img
+              width="28"
+              height="28"
+              src="https://img.icons8.com/material-outlined/24/goal.png"
+              alt="goal"
+              style={{ marginRight: "10px", color: "white" }}
+            />
+            Goal
+          </Button>
+          <Button
+            variant="contained"
+            sx={{
+              backgroundColor: isActive("/expenses") ? "#D9D9D9" : "#1976D2",
+              color: isActive("/expenses") ? "black" : "white",
+              fontWeight: 1000,
+              margin: "10px",
+            }}
+            component={Link}
+            to={"/expenses"}
+          >
+            <img
+              width="28"
+              height="28"
+              src="https://img.icons8.com/ios-filled/50/1A1A1A/request-money.png"
+              alt="request-money"
+              style={{ marginRight: "10px" }}
+            />
+            Expense
+          </Button>
+        </Box>
       </Drawer>
     </Box>
   );

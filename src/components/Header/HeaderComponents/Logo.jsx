fa5af30895c8cdb0fa013f19550a9bb13da1b28a
Beteab Admassu <beteab.admassu.tadele@gmail.com>
2023-09-13 15:12:07 +0300

    navBar_issue_fixed

--- src/components/Header/HeaderComponents/Logo.jsx
+++ src/components/Header/HeaderComponents/Logo.jsx
@@ -1,48 +1,40 @@
 import { Box, Typography } from "@mui/material";
 
 const Logo = () => {
-    return (
+  return (
+    <Box>
       <Box
         sx={{
-          width: "34vw",
-          height: "100%",
           display: "flex",
-          justifyContent: "left",
-          alignItems: "inherit",
-          paddingLeft: "3vw",
+          height: "100%",
+          alignItems: "center",
         }}
       >
-        <Box
+        <div
+          style={{
+            width: "25px",
+            height: "25px",
+            backgroundColor: "white",
+            borderRadius: "50%",
+            marginBottom: "5vh",
+            marginRight: "1.5vh",
+          }}
+        ></div>
+
+        <Typography
+          variant="h4"
           sx={{
-            display: "flex",
-            height: "100%",
-            alignItems: "center",
+            fontWeight: 600,
+            lineHeight: "25px",
+            color: "white",
           }}
         >
-          <div
-            style={{
-              width: "25px",
-              height: "25px",
-              backgroundColor: "white",
-              borderRadius: "50%",
-              marginBottom: "5vh",
-              marginRight: "1.5vh",
-            }}
-          ></div>
-  
-          <Typography
-            variant="h4"
-            sx={{
-              fontWeight: 600,
-              lineHeight: "25px",
-            }}
-          >
-            {" "}
-            finuncle
-          </Typography>
-        </Box>
+          {" "}
+          finuncle
+        </Typography>
       </Box>
-    );
-  };
+    </Box>
+  );
+};
 
-  export default Logo;
+export default Logo;

5ebbe2450f7a572164956c5d61b5449bba7aa8e4
= <rohitbhatwara1@gmail.com>
2023-09-21 21:45:00 +0530

    feat(currency-preference): using country-data-codes to get list of currencies and showing in selector

--- src/pages/Settings/Settings.jsx
+++ src/pages/Settings/Settings.jsx
@@ -6,11 +6,27 @@ import {
   Select,
 } from "@mui/material";
 import styles from "./Settings.module.css";
-import { CURRENCY_PREFERENCES } from "utils/Settings";
 import { useUserPreferences } from "hooks/useUserPreferences";
+import { getCountryList } from "country-data-codes";
+import { useMemo } from "react";
+import { uniqBy } from "lodash";
 
 export default function Settings() {
   const { preferences, setPreferences, savePreferences } = useUserPreferences();
+
+  const currencyCodes = useMemo(() => {
+    const countryList = getCountryList();
+    const currencyList = countryList
+      .map((item) => ({
+        name: item.currency.name,
+        symbol: item.currency.symbol,
+      }))
+      .filter((item) => Boolean(item.symbol));
+
+    return uniqBy(currencyList, (currency) => currency.symbol);
+  }, []);
+
+  console.log(currencyCodes);
 
   const handleValueChange = (e) => {
     setPreferences((prev) => ({ ...prev, [e.target.name]: e.target.value }));
@@ -30,8 +46,10 @@ export default function Settings() {
             label="Currency Unit"
             onChange={handleValueChange}
           >
-            {Object.entries(CURRENCY_PREFERENCES).map(([label, value]) => (
-              <MenuItem value={value}>{label}</MenuItem>
+            {currencyCodes.map(({ name, symbol }) => (
+              <MenuItem value={symbol}>
+                {name} ({symbol})
+              </MenuItem>
             ))}
           </Select>
         </FormControl>

feb4f56cf49753d6858f9d4816474d5c988dc8f4
= <rohitbhatwara1@gmail.com>
2023-09-26 22:33:13 +0530

    refactor(links): update terms and conditions link and create pages for the same

---
+++ src/pages/PrivacyPolicies/PrivacyPolicies.jsx
@@ -0,0 +1,7 @@
+export default function PrivacyPolicies() {
+  return (
+    <div className="d-flex align-items-center justify-content-center min-vh-100 h1 text-muted font-weight-bold">
+      Privacy and policies page is under implementation
+    </div>
+  );
+}

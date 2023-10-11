c5c6dbf2669f34e6149e71ae4ebde874f3a338e1
= <rohitbhatwara1@gmail.com>
2023-10-11 20:56:31 +0530

    refactor(income-expense): remove unnecessary Typography

--- src/pages/income-statement/IncomeStatementPage.jsx
+++ src/pages/income-statement/IncomeStatementPage.jsx
@@ -18,11 +18,7 @@ function CustomTabPanel(props) {
       aria-labelledby={`simple-tab-${index}`}
       {...other}
     >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
     </div>
   );
 }

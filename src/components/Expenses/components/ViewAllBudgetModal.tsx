958128d04a3f8f71c627366142787329773af048
= <rohitbhatwara1@gmail.com>
2023-10-15 20:07:17 +0530

    feat(budget): add loading state in view all budget modal

--- src/components/Expenses/components/ViewAllBudgetModal.tsx
+++ src/components/Expenses/components/ViewAllBudgetModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react'
 import { Delete } from '@mui/icons-material'
 import {
   Button,
@@ -16,19 +16,57 @@ import customParseFormat from 'dayjs/plugin/customParseFormat'
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import { deleteBudget, fetchBudgets } from 'queries/budget'
 import { BUDGET_QUERY_KEYS } from 'utils/query-keys'
+import BeatLoader from 'react-spinners/BeatLoader'
 import EditBudgetModal from './EditBudgetModal'
+
 dayjs.extend(customParseFormat)
 
 export default function ViewAllBudgetModal() {
   const [open, setOpen] = useState(false)
   const queryClient = useQueryClient()
 
-  const { data } = useQuery([BUDGET_QUERY_KEYS.BUDGETS], fetchBudgets, { enabled: !!open })
+  const { data, isLoading } = useQuery([BUDGET_QUERY_KEYS.BUDGETS], fetchBudgets, { enabled: !!open })
   const deleteBudgetMutation = useMutation(deleteBudget, {
     onSuccess: () => {
       queryClient.invalidateQueries([BUDGET_QUERY_KEYS.BUDGETS])
     },
   })
+
+  const content = useMemo(() => {
+    if (isLoading) {
+      return (
+        <div style={{ width: '33rem', height: '10rem' }} className='d-flex align-items-center justify-content-center'>
+          <BeatLoader color='#1976D2' />
+        </div>
+      )
+    }
+
+    return (
+      <Table style={{ minWidth: '33rem' }}>
+        <TableBody>
+          {data?.map((budget) => (
+            <TableRow key={budget.id}>
+              <TableCell>{dayjs(budget.month, 'YYYY-MM-DD').format('MMMM')}</TableCell>
+              <TableCell>{budget.limit}</TableCell>
+              <TableCell>
+                <EditBudgetModal budget={budget} />
+                <IconButton
+                  disabled={deleteBudgetMutation.isLoading}
+                  aria-label='delete'
+                  color='error'
+                  onClick={() => {
+                    deleteBudgetMutation.mutate(budget.id)
+                  }}
+                >
+                  <Delete />
+                </IconButton>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    )
+  }, [data, deleteBudgetMutation, isLoading])
 
   return (
     <>
@@ -48,31 +86,7 @@ export default function ViewAllBudgetModal() {
         }}
       >
         <DialogTitle>Budgets</DialogTitle>
-        <DialogContent>
-          <Table style={{ minWidth: '33rem' }}>
-            <TableBody>
-              {data?.map((budget) => (
-                <TableRow key={budget.id}>
-                  <TableCell>{dayjs(budget.month, 'YYYY-MM-DD').format('MMMM')}</TableCell>
-                  <TableCell>{budget.limit}</TableCell>
-                  <TableCell>
-                    <EditBudgetModal budget={budget} />
-                    <IconButton
-                      disabled={deleteBudgetMutation.isLoading}
-                      aria-label='delete'
-                      color='error'
-                      onClick={() => {
-                        deleteBudgetMutation.mutate(budget.id)
-                      }}
-                    >
-                      <Delete />
-                    </IconButton>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </DialogContent>
+        <DialogContent>{content}</DialogContent>
       </Dialog>
     </>
   )

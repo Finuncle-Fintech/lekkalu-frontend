b97b886f03ff589c03a134faa441473a87aca163
= <rohitbhatwara1@gmail.com>
2023-10-20 19:08:43 +0530

    refactor(auth-protection): add loading state in auth protection component

--- src/components/AuthProtection/AuthProtection.tsx
+++ src/components/AuthProtection/AuthProtection.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useLocation, Navigate } from 'react-router-dom'
+import { LoaderIcon } from 'lucide-react'
 import { useAuthContext } from '@/hooks/use-auth'
 
 type Props = {
@@ -8,7 +9,16 @@ type Props = {
 
 export default function AuthProtection({ children }: Props) {
   const location = useLocation()
-  const { tokenData } = useAuthContext()
+  const { isAuthenticationInProgress, tokenData } = useAuthContext()
+
+  if (isAuthenticationInProgress) {
+    return (
+      <div className='w-full h-screen z-40 flex items-center justify-center gap-2'>
+        <LoaderIcon className='w-4 h-4 animate-spin' />
+        <div>Loading please wait...</div>
+      </div>
+    )
+  }
 
   if (tokenData) {
     return children

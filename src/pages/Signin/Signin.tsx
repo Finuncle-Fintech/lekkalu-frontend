95fc797ea0763f0046c5529907da489337ebdc96
= <rohitbhatwara1@gmail.com>
2023-10-24 23:56:56 +0530

    style(login-signup): update login and signup page styles

--- src/pages/Signin/Signin.tsx
+++ src/pages/Signin/Signin.tsx
@@ -13,6 +13,7 @@ import Password from '@/components/ui/password'
 import { Checkbox } from '@/components/ui/checkbox'
 import { Button } from '@/components/ui/button'
 import { useAuthContext } from '@/hooks/use-auth'
+import Page from '@/components/Page/Page'
 
 export const Signin = () => {
   const { tokenData, loginMutation } = useAuthContext()
@@ -37,107 +38,109 @@ export const Signin = () => {
   }
 
   return (
-    <div className='h-screen bg-primary flex items-center justify-around'>
-      <div className='flex-1 pt-4 hidden md:flex items-center justify-center'>
-        <div className='relative'>
-          <div className='absolute -top-10 -left-10 bg-white rounded-full w-10 h-10' />
-          <p className='text-white text-2xl font-medium text-center'>finuncle</p>
+    <div className='bg-primary'>
+      <Page className='flex items-center justify-around h-screen'>
+        <div className='flex-1 pt-4 hidden md:flex items-center justify-center'>
+          <div className='relative'>
+            <div className='absolute -top-10 -left-10 bg-white rounded-full w-10 h-10' />
+            <p className='text-white text-2xl font-medium text-center'>finuncle</p>
+          </div>
         </div>
-      </div>
 
-      <div className='flex-1 max-w-xl mx-auto'>
-        <div className='bg-white p-4 m-4 rounded-lg'>
-          <h1 className='text-2xl font-bold'>Log in</h1>
-          <p className='text-muted-foreground text-sm mb-4'>Welcome back to finuncle!</p>
+        <div className='flex-1 max-w-xl mx-auto'>
+          <div className='bg-white p-4 m-4 rounded-lg'>
+            <h1 className='text-2xl font-bold'>Log in</h1>
+            <p className='text-muted-foreground text-sm mb-4'>Welcome back to finuncle!</p>
 
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(handleSignin)} className='flex flex-col gap-4'>
-              <FormField
-                disabled={loginMutation.isLoading}
-                control={form.control}
-                name='username'
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Username</FormLabel>
-                    <FormControl>
-                      <Input placeholder='Enter your username' {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(handleSignin)} className='flex flex-col gap-4'>
+                <FormField
+                  disabled={loginMutation.isLoading}
+                  control={form.control}
+                  name='username'
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Username</FormLabel>
+                      <FormControl>
+                        <Input placeholder='Enter your username' {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
 
-              <FormField
-                disabled={loginMutation.isLoading}
-                control={form.control}
-                name='password'
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Password</FormLabel>
-                    <FormControl>
-                      <Password placeholder='Enter your password' {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+                <FormField
+                  disabled={loginMutation.isLoading}
+                  control={form.control}
+                  name='password'
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <Password placeholder='Enter your password' {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
 
-              <FormField
-                disabled={loginMutation.isLoading}
-                control={form.control}
-                name='rememberMe'
-                render={({ field }) => (
-                  <FormItem>
-                    <FormControl>
-                      <div className='flex items-center space-x-2'>
-                        <Checkbox id='rememberMe' checked={field.value} onCheckedChange={field.onChange} />
-                        <label
-                          htmlFor='rememberMe'
-                          className='text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
-                        >
-                          Remember Me
-                        </label>
-                      </div>
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+                <FormField
+                  disabled={loginMutation.isLoading}
+                  control={form.control}
+                  name='rememberMe'
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <div className='flex items-center space-x-2'>
+                          <Checkbox id='rememberMe' checked={field.value} onCheckedChange={field.onChange} />
+                          <label
+                            htmlFor='rememberMe'
+                            className='text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
+                          >
+                            Remember Me
+                          </label>
+                        </div>
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
 
-              <Button>Continue</Button>
-            </form>
-          </Form>
+                <Button>Continue</Button>
+              </form>
+            </Form>
 
-          <Link to='/signup' className='block text-sm text-muted-foreground my-2'>
-            Don&apos;t have an account? Sign Up
-          </Link>
+            <Link to='/signup' className='block text-sm text-muted-foreground my-2'>
+              Don&apos;t have an account? Sign Up
+            </Link>
 
-          <When truthy={!isUnderDevelopment}>
-            <div className='space-y-4'>
-              <div className='flex items-center gap-2'>
-                <div className='h-[1px] bg-muted w-full' />
-                <p>OR</p>
-                <div className='h-[1px] bg-muted w-full' />
+            <When truthy={!isUnderDevelopment}>
+              <div className='space-y-4'>
+                <div className='flex items-center gap-2'>
+                  <div className='h-[1px] bg-muted w-full' />
+                  <p>OR</p>
+                  <div className='h-[1px] bg-muted w-full' />
+                </div>
+
+                <Button className='w-full' variant='outline'>
+                  <FacebookIcon className='mr-2 w-4 h-4' />
+                  <span>Continue with Facebook</span>
+                </Button>
+                <Button className='w-full' variant='outline'>
+                  <Google className='mr-2 w-4 h-4' />
+                  <span>Continue with Google</span>
+                </Button>
+
+                {/* @TODO: Update Apple icon */}
+                <Button className='w-full' variant='outline'>
+                  <AppleIcon className='mr-2 w-4 h-4' />
+                  <span>Continue with Apple</span>
+                </Button>
               </div>
-
-              <Button className='w-full' variant='outline'>
-                <FacebookIcon className='mr-2 w-4 h-4' />
-                <span>Continue with Facebook</span>
-              </Button>
-              <Button className='w-full' variant='outline'>
-                <Google className='mr-2 w-4 h-4' />
-                <span>Continue with Google</span>
-              </Button>
-
-              {/* @TODO: Update Apple icon */}
-              <Button className='w-full' variant='outline'>
-                <AppleIcon className='mr-2 w-4 h-4' />
-                <span>Continue with Apple</span>
-              </Button>
-            </div>
-          </When>
+            </When>
+          </div>
         </div>
-      </div>
+      </Page>
     </div>
   )
 }
--- src/pages/Signup/Signup.tsx
+++ src/pages/Signup/Signup.tsx
@@ -9,6 +9,7 @@ import Password from '@/components/ui/password'
 import { Checkbox } from '@/components/ui/checkbox'
 import { Button } from '@/components/ui/button'
 import { useAuthContext } from '@/hooks/use-auth'
+import Page from '@/components/Page/Page'
 
 export default function Signup() {
   const { tokenData, signupMutation } = useAuthContext()
@@ -35,120 +36,127 @@ export default function Signup() {
   }
 
   return (
-    <div className='grid md:grid-cols-2 h-[80vh]'>
-      <div className='items-center justify-center hidden md:flex gap-2'>
-        <div>
-          <div className='w-5 h-5 bg-primary rounded-full' />
-          <div className='text-2xl font-bold'>finuncle</div>
+    <div className='bg-primary'>
+      <Page className='flex items-center justify-around h-screen'>
+        <div className='flex-1 pt-4 hidden md:flex items-center justify-center'>
+          <div className='relative'>
+            <div className='absolute -top-10 -left-10 bg-white rounded-full w-10 h-10' />
+            <p className='text-white text-2xl font-medium text-center'>finuncle</p>
+          </div>
         </div>
-      </div>
-      <div className='flex items-center justify-center p-4'>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(handleSignup)} className='space-y-4 border rounded p-4 w-full'>
-            <FormField
-              disabled={signupMutation.isLoading}
-              control={form.control}
-              name='username'
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Username</FormLabel>
-                  <FormControl>
-                    <Input placeholder='Enter your username' {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              disabled={signupMutation.isLoading}
-              control={form.control}
-              name='email'
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Email</FormLabel>
-                  <FormControl>
-                    <Input placeholder='Enter your email' {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
 
-            <FormField
-              disabled={signupMutation.isLoading}
-              control={form.control}
-              name='password'
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Password</FormLabel>
-                  <FormControl>
-                    <Password placeholder='Enter your password' {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+        <div className='flex-1 max-w-xl mx-auto'>
+          <div className='bg-white p-4 m-4 rounded-lg'>
+            <h1 className='text-2xl font-bold mb-4'>Sign Up</h1>
 
-            <FormField
-              disabled={signupMutation.isLoading}
-              control={form.control}
-              name='termsAndConditions'
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <div className='flex items-center space-x-2'>
-                      <Checkbox id='termsAndConditions' checked={field.value} onCheckedChange={field.onChange} />
-                      <label
-                        htmlFor='termsAndConditions'
-                        className='text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
-                      >
-                        I have read, understood and agreed to{' '}
-                        <Link to='/terms-and-conditions' className='underline'>
-                          Finuncle&apos;s Terms and Conditions
-                        </Link>
-                      </label>
-                    </div>
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(handleSignup)} className='space-y-4 w-full'>
+                <FormField
+                  disabled={signupMutation.isLoading}
+                  control={form.control}
+                  name='username'
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Username</FormLabel>
+                      <FormControl>
+                        <Input placeholder='Enter your username' {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  disabled={signupMutation.isLoading}
+                  control={form.control}
+                  name='email'
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input placeholder='Enter your email' {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
 
-            <FormField
-              disabled={signupMutation.isLoading}
-              control={form.control}
-              name='privacyPolicy'
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <div className='flex items-center space-x-2'>
-                      <Checkbox id='privacyPolicy' checked={field.value} onCheckedChange={field.onChange} />
-                      <label
-                        htmlFor='privacyPolicy'
-                        className='text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
-                      >
-                        I have read, understood and agreed to{' '}
-                        <Link to='/privacy-policies' className='underline'>
-                          Finuncle&apos;s Privacy Policy
-                        </Link>
-                      </label>
-                    </div>
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+                <FormField
+                  disabled={signupMutation.isLoading}
+                  control={form.control}
+                  name='password'
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <Password placeholder='Enter your password' {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
 
-            <Button type='submit' loading={signupMutation.isLoading}>
-              Continue
-            </Button>
+                <FormField
+                  disabled={signupMutation.isLoading}
+                  control={form.control}
+                  name='termsAndConditions'
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <div className='flex items-center space-x-2'>
+                          <Checkbox id='termsAndConditions' checked={field.value} onCheckedChange={field.onChange} />
+                          <label
+                            htmlFor='termsAndConditions'
+                            className='text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
+                          >
+                            I have read, understood and agreed to{' '}
+                            <Link to='/terms-and-conditions' className='underline'>
+                              Finuncle&apos;s Terms and Conditions
+                            </Link>
+                          </label>
+                        </div>
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
 
-            <Link to='/signin' className='block text-sm text-muted-foreground my-2'>
-              Already have an account? Login
-            </Link>
-          </form>
-        </Form>
-      </div>
+                <FormField
+                  disabled={signupMutation.isLoading}
+                  control={form.control}
+                  name='privacyPolicy'
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <div className='flex items-center space-x-2'>
+                          <Checkbox id='privacyPolicy' checked={field.value} onCheckedChange={field.onChange} />
+                          <label
+                            htmlFor='privacyPolicy'
+                            className='text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
+                          >
+                            I have read, understood and agreed to{' '}
+                            <Link to='/privacy-policies' className='underline'>
+                              Finuncle&apos;s Privacy Policy
+                            </Link>
+                          </label>
+                        </div>
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <Button type='submit' loading={signupMutation.isLoading}>
+                  Continue
+                </Button>
+
+                <Link to='/signin' className='block text-sm text-muted-foreground my-2'>
+                  Already have an account? Login
+                </Link>
+              </form>
+            </Form>
+          </div>
+        </div>
+      </Page>
     </div>
   )
 }

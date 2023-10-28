5859afcd2db972446ee51ea208b950d6d9c92175
Rohit Kumar Saini <40729749+rockingrohit9639@users.noreply.github.com>
2023-10-28 21:18:51 +0530

    Refactor/income statement v2 (#308)
    * fix(signin-&-signup): fix styling issues on signin and signup pages
    
    * refactor(links): update terms and conditions link and create pages for the same
    
    * feat(eye-buttons): add eye buttons on password fields in signin and signup pages
    
    * refactor(login-signup): add client side validations in login and signup form
    
    * refactor(login-signup): commented social login buttons
    
    * refactor(login-signup): update social logins with isUnderDevelopment variable
    
    * refactor(login-signup): increasepassword length
    
    * feat(signin): add test cases for signin
    
    * feat(signup): add test cases for signup
    
    * refactor(income-statement): migrated mui data-grid to shadcn ui
    
    * style(login-signup): update login and signup page styles
    
    * refactor(app): remove ununsed dependencies
    
    * refactor(balance-sheet): update balance sheet chart component and remove unused dependencies

--- src/pages/BalanceSheet/BalanceSheet.tsx
+++ src/pages/BalanceSheet/BalanceSheet.tsx
@@ -1,67 +1,17 @@
-import React, { useMemo, useState } from 'react'
-import { useQuery } from '@tanstack/react-query'
+import React from 'react'
 import Page from '@/components/Page/Page'
 import AssetsTable from './components/AssetsTable'
 import LiabilitiesTable from './components/LiabilitiesTable'
-import { BALANCE_SHEET } from '@/utils/query-keys'
-import { fetchLiabilities, fetchPhysicalAssets } from '@/queries/balance-sheet'
-
-// @ts-expect-error (no-type-definitions)
-import GraphCard from '@/components/BalanceSheet/GraphCard'
-// @ts-expect-error (no-type-definitions)
-import BarGraph from '@/components/BalanceSheet/BarGraph'
+import AssetLiabilitiesChart from './components/AssetLiabilitiesCharts'
+import LiabilityBarGraph from './components/LiabilityBarGraph'
 
 export default function BalanceSheet() {
-  const { data: physicalAssets } = useQuery([BALANCE_SHEET.ASSETS], fetchPhysicalAssets)
-  const { data: liabilities } = useQuery([BALANCE_SHEET.LIABILITIES], fetchLiabilities)
-
-  const [barGraphIsOpen, setBarGraphIsOpen] = useState(false)
-
-  const assetsData = useMemo(() => {
-    if (!physicalAssets) {
-      return {}
-    }
-
-    const totalVal = physicalAssets.reduce((acc, curr) => (acc += parseFloat(curr.market_value)), 0.00001)
-    const finalAssets = physicalAssets.map((asset) => ({ ...asset, value: parseFloat(asset.market_value) }))
-
-    return { totalVal, finalAssets }
-  }, [physicalAssets])
-
-  const liabilitiesData = useMemo(() => {
-    if (!liabilities) {
-      return {}
-    }
-
-    const totalVal = liabilities.reduce((acc, curr) => (acc += parseFloat(curr.balance)), 0.00001)
-    const finalLiabilities = liabilities.map((liability) => ({
-      ...liability,
-      value: parseFloat(liability.balance),
-      principal: parseFloat(liability.principal),
-      interest: parseFloat(liability.interest_rate),
-      tenure: liability.tenure,
-      closure_charges: parseFloat(liability.closure_charges),
-      disbursement_date: liability.disbursement_date,
-    }))
-
-    return { totalVal, finalLiabilities }
-  }, [liabilities])
-
-  const handleOpen = () => {
-    setBarGraphIsOpen(true)
-  }
-  const handleClose = () => {
-    setBarGraphIsOpen(false)
-  }
-
   return (
-    <Page>
-      <GraphCard assetDatas={assetsData} liabilityDatas={liabilitiesData} setBarGraphIsOpen={handleOpen} />
-      <BarGraph
-        setBarGraphIsOpen={handleClose}
-        barGraphIsOpen={barGraphIsOpen}
-        dataLiability={liabilitiesData.finalLiabilities ?? []}
-      />
+    <Page className='space-y-4'>
+      <div className='text-2xl font-bold'>Balance Sheet</div>
+      <AssetLiabilitiesChart />
+
+      <LiabilityBarGraph />
 
       <div className='text-2xl font-bold my-2'>Assets</div>
       <AssetsTable />

c5b7f15e2144c2356670f0cc8821d2f7630e84aa
= <rohitbhatwara1@gmail.com>
2023-10-28 22:03:17 +0530

    refactor(income-statement): update api client for income statement

--- src/queries/income-statement.ts
+++ src/queries/income-statement.ts
@@ -1,43 +1,43 @@
 import { AddIncomeStatementSchema } from '@/schema/income-statement'
 import { IncomeStatement } from '@/types/income-statement'
-import { apiClient } from '@/utils/client'
+import { v1ApiClient } from '@/utils/client'
 
 export async function fetchIncomeSources() {
-  const { data } = await apiClient.get<Array<IncomeStatement & { isNew: boolean }>>('income_source/')
+  const { data } = await v1ApiClient.get<Array<IncomeStatement & { isNew: boolean }>>('income_source/')
   return data
 }
 
 export async function fetchIncomeExpenses() {
-  const { data } = await apiClient.get<Array<IncomeStatement & { isNew: boolean }>>('income_expense/')
+  const { data } = await v1ApiClient.get<Array<IncomeStatement & { isNew: boolean }>>('income_expense/')
   return data
 }
 
 export async function createIncomeSource(dto: AddIncomeStatementSchema) {
-  const { data } = await apiClient.post('income_source/', dto)
+  const { data } = await v1ApiClient.post('income_source/', dto)
   return data
 }
 
 export async function updateIncomeSource(id: number, dto: Partial<AddIncomeStatementSchema>) {
-  const { data } = await apiClient.put(`income_source/${id}`, dto)
+  const { data } = await v1ApiClient.put(`income_source/${id}`, dto)
   return data
 }
 
 export async function deleteIncomeSource(id: number) {
-  const { data } = await apiClient.delete(`income_source/${id}`)
+  const { data } = await v1ApiClient.delete(`income_source/${id}`)
   return data
 }
 
 export async function createIncomeExpense(dto: AddIncomeStatementSchema) {
-  const { data } = await apiClient.post('income_expense/', dto)
+  const { data } = await v1ApiClient.post('income_expense/', dto)
   return data
 }
 
 export async function updateIncomeExpense(id: number, dto: Partial<AddIncomeStatementSchema>) {
-  const { data } = await apiClient.put(`income_expense/${id}`, dto)
+  const { data } = await v1ApiClient.put(`income_expense/${id}`, dto)
   return data
 }
 
 export async function deleteIncomeExpense(id: number) {
-  const { data } = await apiClient.delete(`income_expense/${id}`)
+  const { data } = await v1ApiClient.delete(`income_expense/${id}`)
   return data
 }

9d3f53702a72f4fcfb63a1a2c0f11f29b3a46d09
= <rohitbhatwara1@gmail.com>
2023-10-31 00:15:49 +0530

    feat(goals): add very basic goal details page

---
+++ src/pages/GoalDetails/GoalDetails.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { TimerIcon } from 'lucide-react'
+import Page from '@/components/Page/Page'
+
+export default function GoalDetails() {
+  return (
+    <Page>
+      <h1 className='text-2xl font-bold mb-8'>Title of the goal</h1>
+
+      <div className='grid md:grid-cols-2 gap-4'>
+        <div className='flex'>
+          <div className='flex gap-2 flex-1'>
+            <TimerIcon className='w-4 h-4' />
+            <div>Observer Field</div>
+          </div>
+          <div className='flex-1 font-medium'>Interest Rate</div>
+        </div>
+
+        <div className='flex'>
+          <div className='flex gap-2 flex-1'>
+            <TimerIcon className='w-4 h-4' />
+            <div>Degree</div>
+          </div>
+          <div className='flex-1 font-medium'>Higher</div>
+        </div>
+
+        <div className='flex'>
+          <div className='flex gap-2 flex-1'>
+            <TimerIcon className='w-4 h-4' />
+            <div>Actual Value</div>
+          </div>
+          <div className='flex-1 font-medium'>5 %</div>
+        </div>
+      </div>
+    </Page>
+  )
+}

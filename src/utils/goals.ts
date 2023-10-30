f4a8574e46ef90d50911ff6a28a002930e8c04f7
= <rohitbhatwara1@gmail.com>
2023-10-31 00:04:39 +0530

    refactor(goals): update fields in goals page

--- src/utils/goals.ts
+++ src/utils/goals.ts
@@ -7,62 +7,100 @@ export const GOAL_INPUTS: InputField[] = [
     type: 'text',
   },
   {
-    id: 'target_metric',
-    label: 'Target Metric',
-    type: 'number',
-  },
-  {
-    id: 'current_metric',
-    label: 'Current Metric',
-    type: 'number',
-  },
-  {
-    id: 'reachability_in_months',
-    label: 'Reachability in Months',
-    type: 'number',
-  },
-  {
-    id: 'reachability_in_years',
-    label: 'Reachability in Years',
-    type: 'number',
-  },
-  {
-    id: 'started',
-    label: 'Choose the Started Date',
-    type: 'date',
-  },
-  {
-    id: 'finished',
-    label: 'Choose the Finished Date',
-    type: 'date',
-  },
-  {
-    id: 'planned_start',
-    label: 'Choose the Planned Start Date',
-    type: 'date',
-  },
-  {
-    id: 'planned_finish',
-    label: 'Choose the Planned Finish Date',
-    type: 'date',
-  },
-  {
-    id: 'comments',
-    label: 'Provide comments for goal',
-    type: 'text',
-  },
-  {
-    id: 'balance',
-    label: 'Balance',
-    type: 'number',
-  },
-  {
-    id: 'prefered_value_of_balance',
-    label: 'Choose preferred value of Balance',
-    type: 'radio',
+    id: 'observerField',
+    label: 'Observer Field',
+    type: 'select',
     options: [
-      { id: 'H', label: 'High' },
-      { id: 'L', label: 'Low' },
+      {
+        id: 'interest_rate',
+        label: 'Interest Rate',
+      },
+    ],
+  },
+  {
+    id: 'degree',
+    label: 'Degree',
+    type: 'select',
+    options: [
+      { id: 'HIGHER', label: 'Higher' },
+      { id: 'LOWER', label: 'Lower' },
+    ],
+  },
+  {
+    id: 'actualValue',
+    label: 'Actual Value',
+    type: 'number',
+  },
+  {
+    id: 'unit',
+    label: 'Unit',
+    type: 'select',
+    options: [
+      {
+        id: 'percent',
+        label: 'Percentage (%)',
+      },
     ],
   },
 ]
+
+// * These fields are on hold for now
+// {
+//   id: 'target_metric',
+//   label: 'Target Metric',
+//   type: 'number',
+// },
+// {
+//   id: 'current_metric',
+//   label: 'Current Metric',
+//   type: 'number',
+// },
+// {
+//   id: 'reachability_in_months',
+//   label: 'Reachability in Months',
+//   type: 'number',
+// },
+// {
+//   id: 'reachability_in_years',
+//   label: 'Reachability in Years',
+//   type: 'number',
+// },
+// {
+//   id: 'started',
+//   label: 'Choose the Started Date',
+//   type: 'date',
+// },
+// {
+//   id: 'finished',
+//   label: 'Choose the Finished Date',
+//   type: 'date',
+// },
+// {
+//   id: 'planned_start',
+//   label: 'Choose the Planned Start Date',
+//   type: 'date',
+// },
+// {
+//   id: 'planned_finish',
+//   label: 'Choose the Planned Finish Date',
+//   type: 'date',
+// },
+// {
+//   id: 'comments',
+//   label: 'Provide comments for goal',
+//   type: 'text',
+// },
+// {
+//   id: 'balance',
+//   label: 'Balance',
+//   type: 'number',
+// },
+// {
+//   id: 'prefered_value_of_balance',
+//   label: 'Choose preferred value of Balance',
+//   type: 'radio',
+//   options: [
+//     { id: 'H', label: 'High' },
+//     { id: 'L', label: 'Low' },
+//   ],
+// },

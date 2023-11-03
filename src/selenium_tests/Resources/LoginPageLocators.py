d6a77bedb0539ec6afc194de087a990cea48bd92
SzatmariA <szatma@inf.u-szeged.hu>
2023-11-03 15:53:58 +0100

    remove tests because they were moved to another repository https://github.com/GVR-Secure-Solutions/lekkalu-test-frontend

--- src/selenium_tests/Resources/LoginPageLocators.py
+++
@@ -1,9 +0,0 @@
-from selenium.webdriver.common.by import By
-
-
-class LoginPageLocators():
-    get_started_link = (By.XPATH, "/html/body/div/div/div[1]/header/button")
-    login_button = "/html/body/div/div/div[1]/div[2]/div/a[1]"
-    user_name = (By.ID, "username")
-    password = (By.ID, "password")
-    continue_button = "/html/body/div/div/div[2]/div[2]/div[1]/form/button"

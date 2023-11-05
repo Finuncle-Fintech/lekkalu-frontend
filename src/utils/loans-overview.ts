b014bc0c55ac4887be32e1263b8b65acfc750abd
= <rohitbhatwara1@gmail.com>
2023-11-06 00:22:51 +0530

    feat(loans-overview): update logic behind loans overview chart

--- src/utils/loans-overview.ts
+++ src/utils/loans-overview.ts
@@ -1,50 +1,75 @@
 import dayjs from 'dayjs'
 import { round } from 'lodash'
-import { Liability } from '@/types/balance-sheet'
-import { TRANSACTIONS } from '@/data/transactions'
+import { Liability, LoanTransaction } from '@/types/balance-sheet'
+import { fetchTransactionsForLoan } from '@/queries/balance-sheet'
 
 export type Durations = (typeof DURATIONS)[number]['id']
 
 export const DURATIONS = [
   {
-    id: 'LAST_THREE_MONTHS',
-    label: 'Last Three Months',
-    months: getLastMonths(3),
+    id: 'NEXT_THREE_MONTHS',
+    label: 'Next Three Months',
+    months: getNextMonths(3),
   },
   {
-    id: 'LAST_SIX_MONTHS',
-    label: 'Last Six Months',
-    months: getLastMonths(6),
+    id: 'NEXT_SIX_MONTHS',
+    label: 'Next Six Months',
+    months: getNextMonths(6),
   },
   {
-    id: 'LAST_1_YEAR',
-    label: 'Last One Years',
-    months: getLastMonths(12),
+    id: 'NEXT_1_YEAR',
+    label: 'Next One Years',
+    months: getNextMonths(12),
+  },
+  {
+    id: 'NEXT_2_YEAR',
+    label: 'Next Two Years',
+    months: getNextMonths(24),
   },
 ] as const
 
-function getLastMonths(count: number) {
+function getNextMonths(count: number) {
   return Array.from({ length: count }, (_, i) =>
     dayjs()
-      .subtract(i + 1, 'month')
+      .add(i + 2, 'month')
       .endOf('month'),
   )
 }
 
-export function getLoanOverviewData(loans: Liability[], months: dayjs.Dayjs[]) {
+export async function getLoanOverviewData(loans: Liability[], months: dayjs.Dayjs[]) {
   const totalPrincipal = loans.reduce((acc, curr) => (acc += Number(curr.principal)), 0)
+  const transactions: (LoanTransaction & { balance: number })[] = []
+
+  /** Getting all the transactions for all loans */
+  for (const loan of loans) {
+    let balance = Number(loan.principal)
+    const fetchedTransactions = await fetchTransactionsForLoan(loan.id)
+
+    /** calculating balance for transaction */
+    for (const transaction of fetchedTransactions) {
+      if (dayjs(transaction.time).format('MMYYYY') === dayjs().format('MMYYYY')) {
+        continue
+      }
+
+      const tnxBalance = round(balance - Number(transaction.amount), 2)
+      transactions.push({ ...transaction, balance: tnxBalance })
+      balance = tnxBalance
+    }
+  }
 
   const calculatedResults: Array<{ month: string; outstandingPercentage: number }> = []
 
+  /** Filtering DEBIT transactions */
+  const debitTransactions = transactions.filter((transaction) => transaction.type === 2)
+
+  /** Calculating outstanding percentage for each month  */
   for (const month of months) {
-    const transactionsForMonth = TRANSACTIONS.filter(
-      (transaction) => transaction.date.format('MMYYYY') === month.format('MMYYYY'),
+    const transactionsForMonth = debitTransactions.filter(
+      (transaction) => dayjs(transaction.time).format('MMYYYY') === month.format('MMYYYY'),
     )
 
-    const totalOutstanding = transactionsForMonth.reduce((acc, curr) => (acc += Number(curr.amount)), 0)
-
+    const totalOutstanding = transactionsForMonth.reduce((acc, curr) => (acc += Number(curr.balance)), 0)
     const outstandingPercentage = round((totalOutstanding / totalPrincipal) * 100, 2)
-
     calculatedResults.push({
       month: month.format('MMM YYYY'),
       outstandingPercentage,

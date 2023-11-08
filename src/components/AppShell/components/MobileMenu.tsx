9754638eb716889ba0140c9b4b554c4860568bb9
Desai Hardik <87645745+imhardikdesai@users.noreply.github.com>
2023-11-08 23:02:51 +0530

    fix: Added Drawer Insted of full screen toogle button
    managed Sidebar code on mobile screen for both Authorize and non-authorize users

--- src/components/AppShell/components/MobileMenu.tsx
+++ src/components/AppShell/components/MobileMenu.tsx
@@ -2,8 +2,8 @@ import React, { useState } from 'react'
 import { LogOutIcon, MenuIcon } from 'lucide-react'
 import { Link } from 'react-router-dom'
 import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from '@/components/ui/sheet'
-import { Button } from '@/components/ui/button'
-import { CALCULATOR_ROUTES, ROUTES } from '@/utils/app-shell'
+import { Button, buttonVariants } from '@/components/ui/button'
+import { CALCULATOR_ROUTES, ROUTES, UN_AUTHENTICATED_ROUTES } from '@/utils/app-shell'
 import NavLink from './NavLink'
 import When from '@/components/When/When'
 import { useAuthContext } from '@/hooks/use-auth'
@@ -15,10 +15,15 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
 import DownloadAllData from '@/components/DownloadAllData/DownloadAllData'
+import { cn } from '@/utils/utils'
 
-export default function MobileMenu() {
+export default function MobileMenu({
+  isUnAuthenticatedHeader
+}:{
+  isUnAuthenticatedHeader?:boolean
+}) {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
-  const { userData, logout } = useAuthContext()
+  const { userData, logout, tokenData } = useAuthContext()
 
   return (
     <Sheet open={isMobileMenuOpen} onOpenChange={setIsMobileMenuOpen}>
@@ -36,7 +41,7 @@ export default function MobileMenu() {
         <div className='flex flex-col justify-between h-full'>
           <div>
             <div className='space-y-1 '>
-              {ROUTES.map((route) => (
+              {(isUnAuthenticatedHeader ? UN_AUTHENTICATED_ROUTES : ROUTES).map((route) => (
                 <NavLink key={route.path} to={route.path} label={route.label} icon={route.icon} />
               ))}
             </div>
@@ -49,29 +54,66 @@ export default function MobileMenu() {
             </div>
           </div>
 
-          <div className='space-x-2 pb-12'>
-            <DownloadAllData />
+          <div className="space-x-2 pb-12">
+            {!isUnAuthenticatedHeader && <DownloadAllData />}
 
-            <When truthy={Boolean(userData)}>
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <Button variant='outline'>@{userData?.username}</Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent>
-                  <When truthy={Boolean(userData?.email)}>
-                    <DropdownMenuLabel>{userData?.email}</DropdownMenuLabel>
-                  </When>
+            {isUnAuthenticatedHeader ? (
+              <When
+                truthy={Boolean(tokenData)}
+                fallback={
+                  <div className="space-x-2 flex w-full">
+                    <Link
+                      to="/signin"
+                      className={cn(
+                        buttonVariants({ variant: 'outline' }),
+                        'border-1 border border-black/50',
+                        'w-full',
+                      )}
+                    >
+                      Signin
+                    </Link>
 
-                  <DropdownMenuItem className='cursor-pointer' asChild>
-                    <Link to='/profile'>Profile</Link>
-                  </DropdownMenuItem>
-                  <DropdownMenuItem className='cursor-pointer' onClick={logout}>
-                    <LogOutIcon className='mr-2 h-4 w-4' />
-                    <span>Logout</span>
-                  </DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </When>
+                    <Link
+                      to="/signup"
+                      className={cn(
+                        buttonVariants({ variant: 'secondary' }),
+                        'w-full border-1 border border-black/50',
+                      )}
+                    >
+                      Signup
+                    </Link>
+                  </div>
+                }
+              >
+                <Link
+                  to="/dashboard"
+                  className={cn(buttonVariants({ variant: 'secondary' }), 'w-full')}
+                >
+                  Dashboard
+                </Link>
+              </When>
+            ) : (
+              <When truthy={Boolean(userData)}>
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button variant="outline">@{userData?.username}</Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent>
+                    <When truthy={Boolean(userData?.email)}>
+                      <DropdownMenuLabel>{userData?.email}</DropdownMenuLabel>
+                    </When>
+
+                    <DropdownMenuItem className="cursor-pointer" asChild>
+                      <Link to="/profile">Profile</Link>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem className="cursor-pointer" onClick={logout}>
+                      <LogOutIcon className="mr-2 h-4 w-4" />
+                      <span>Logout</span>
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </When>
+            )}
           </div>
         </div>
       </SheetContent>
--- src/components/AppShell/components/NavLink.tsx
+++ src/components/AppShell/components/NavLink.tsx
@@ -6,17 +6,16 @@ import { cn } from '@/utils/utils'
 type Props = LinkProps & {
   icon?: React.ReactElement<{ className?: string }>
   label: string
-  twClass?: string
 }
 
-export default function NavLink({ to, icon, label, twClass }: Props) {
+export default function NavLink({ to, icon, label }: Props) {
   const location = useLocation()
   const isActive = location.pathname.startsWith(to as string)
 
   return (
     <Link
       to={to}
-      className={cn(buttonVariants({ variant: (isActive ? 'default' : 'ghost') }), 'w-full justify-start gap-2', twClass)}
+      className={cn(buttonVariants({ variant: (isActive ? 'default' : 'ghost') }), 'w-full justify-start gap-2')}
     >
       {icon ? cloneElement(icon, { className: 'w-4 h-4' }) : null}
       <span className='truncate'>{label}</span>

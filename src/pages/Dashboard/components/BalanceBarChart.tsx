34e8567f1205dcfc723b1df2a91e9d148a4a305d
= <rohitbhatwara1@gmail.com>
2023-11-18 23:13:18 +0530

    feat(dashboard): update style of card and charts

--- src/pages/Dashboard/components/BalanceBarChart.tsx
+++ src/pages/Dashboard/components/BalanceBarChart.tsx
@@ -3,6 +3,7 @@ import { Bar, BarChart, Cell, ResponsiveContainer, XAxis, YAxis } from 'recharts
 import colors from 'tailwindcss/colors'
 import { random } from 'lodash'
 import { useUserPreferences } from '@/hooks/use-user-preferences'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 
 const DUMMY_DATA = [
   {
@@ -59,27 +60,31 @@ export default function BalanceBarChart() {
   const { preferences } = useUserPreferences()
 
   return (
-    <div className='h-72 p-4'>
-      <h1 className='text-lg font-bold'>Balance</h1>
-      <p className='text-muted-foreground mb-4'>Last 12 months</p>
+    <Card className='h-96 p-4 shadow-sm'>
+      <CardHeader>
+        <CardTitle>Balance</CardTitle>
+        <CardDescription>Last 12 months</CardDescription>
+      </CardHeader>
 
-      <ResponsiveContainer width='100%' height='75%'>
-        <BarChart data={DUMMY_DATA}>
-          <Bar dataKey='balance' fill={colors.indigo['500']} background>
-            {DUMMY_DATA.map((_, index) => (
-              <Cell key={`cell-${index}`} radius={10} />
-            ))}
-          </Bar>
-          <XAxis dataKey='name' tickLine={false} axisLine={false} />
-          <YAxis
-            tickLine={false}
-            axisLine={false}
-            tickFormatter={(tick) => {
-              return `${preferences.currencyUnit} ${tick}`
-            }}
-          />
-        </BarChart>
-      </ResponsiveContainer>
-    </div>
+      <CardContent className='w-full h-full'>
+        <ResponsiveContainer width='100%' height='75%'>
+          <BarChart data={DUMMY_DATA}>
+            <Bar dataKey='balance' fill={colors.indigo['500']} background>
+              {DUMMY_DATA.map((_, index) => (
+                <Cell key={`cell-${index}`} radius={10} />
+              ))}
+            </Bar>
+            <XAxis dataKey='name' tickLine={false} axisLine={false} />
+            <YAxis
+              tickLine={false}
+              axisLine={false}
+              tickFormatter={(tick) => {
+                return `${preferences.currencyUnit} ${tick}`
+              }}
+            />
+          </BarChart>
+        </ResponsiveContainer>
+      </CardContent>
+    </Card>
   )
 }

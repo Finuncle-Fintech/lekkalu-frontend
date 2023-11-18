8da4e4256ce55e7636a3417794604d252cbe0a96
= <rohitbhatwara1@gmail.com>
2023-11-18 21:37:33 +0530

    feat(dashboard): create stats cards for dashboard

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,18 +1,57 @@
 import React from 'react'
 
+import { Briefcase, PieChartIcon, UsersIcon } from 'lucide-react'
+import colors from 'tailwindcss/colors'
 import { WeeklyChart } from '../../components/Charts/WeeklyChart'
 import { SpentBalanceChart } from '../../components/Charts/SpentBalanceChart'
 import { CumSumChart } from '../../components/Charts/CumSumChart'
 import Page from '@/components/Page/Page'
 import AssetDepreciationChart from '@/components/Charts/AssetDepreciationChart'
+import StatCard from '@/components/StatCard'
 
 const Home = () => {
   return (
-    <Page className='grid md:grid-cols-2 gap-4'>
-      <WeeklyChart />
-      <SpentBalanceChart />
-      <CumSumChart />
-      <AssetDepreciationChart />
+    <Page className='space-y-4'>
+      <div className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+        <StatCard
+          icon={<Briefcase />}
+          title='Total Sales'
+          value={'$ 43530.00'}
+          color={colors.yellow['500']}
+          extraValue='+24%'
+        />
+
+        <StatCard
+          icon={<UsersIcon />}
+          title='Total Income'
+          value={'$ 856530.00'}
+          color={colors.emerald['500']}
+          extraValue='+24 %'
+        />
+
+        <StatCard
+          icon={<Briefcase />}
+          title='Total Sales'
+          value={'$ 43530.00'}
+          color={colors.purple['700']}
+          extraValue='+24 %'
+        />
+
+        <StatCard
+          icon={<PieChartIcon />}
+          title='Total Expenses'
+          value={'$ 64530.00'}
+          color={colors.orange['600']}
+          extraValue='-32%'
+        />
+      </div>
+
+      <div className='grid md:grid-cols-2 gap-4'>
+        <WeeklyChart />
+        <SpentBalanceChart />
+        <CumSumChart />
+        <AssetDepreciationChart />
+      </div>
     </Page>
   )
 }

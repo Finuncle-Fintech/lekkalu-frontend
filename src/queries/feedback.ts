1250abf12bd1f48c4646cbfa9db0634548dbbea5
Mohit <mohitkhatri4272@gmail.com>
2023-11-23 23:52:14 +0530

    fix: added one apiClient without interceptors

--- src/queries/feedback.ts
+++ src/queries/feedback.ts
@@ -1,8 +1,8 @@
 import { FeedbackSchema } from '@/schema/feedback'
-import { v1ApiClient } from '@/utils/client'
+import { v1ApiClientWithoutToken } from '@/utils/client'
 
 export async function submitFeedback(dto: FeedbackSchema) {
-  const { data } = await v1ApiClient.post<{
+  const { data } = await v1ApiClientWithoutToken.post<{
     id: number
     name: string
     email: string

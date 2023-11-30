00eb84195557fb51467a6316301c676f8af0fdbe
= <rohitbhatwara1@gmail.com>
2023-11-30 21:58:22 +0530

    feat(goals): add feature to create a goal

--- src/pages/CreateGoal/CreateGoal.tsx
+++ src/pages/CreateGoal/CreateGoal.tsx
@@ -1,30 +1,38 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { PlusCircle } from 'lucide-react'
+import { useMutation, useQuery } from '@tanstack/react-query'
+import { useNavigate } from 'react-router'
 import Page from '@/components/Page/Page'
 import { AddGoalSchema, addGoalSchema } from '@/schema/goals'
-import { useAuthContext } from '@/hooks/use-auth'
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
 import InputFieldsRenderer from '@/components/InputFieldsRenderer/InputFieldsRenderer'
-import { CATEGORY_CALCULATION_MAP, GOAL_CATEGORIES, GOAL_INPUTS, GoalCategories } from '@/utils/goals'
 import { Button } from '@/components/ui/button'
+import { GOAL_INPUTS } from '@/utils/goals'
+import { BALANCE_SHEET } from '@/utils/query-keys'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
+import { addGoal } from '@/queries/goals'
+import { fetchIncomeExpenses } from '@/queries/income-statement'
+import { useToast } from '@/components/ui/use-toast'
 
 export default function CreateGoal() {
-  const [goalCategory, setGoalCategory] = useState<GoalCategories>('ASSET_QUALITY')
-  const { userData } = useAuthContext()
-
+  const { toast } = useToast()
+  const navigate = useNavigate()
   const form = useForm<AddGoalSchema>({
     resolver: zodResolver(addGoalSchema),
-    defaultValues: {
-      user: userData?.id ?? 1,
-      category: goalCategory,
+  })
+
+  const incomeExpensesQuery = useQuery([BALANCE_SHEET.LIABILITIES], fetchIncomeExpenses)
+  const createGoalMutation = useMutation(addGoal, {
+    onSuccess: () => {
+      toast({ title: 'Goal created successfully!' })
+      navigate('/goals')
     },
   })
 
   const handleGoalCreate = (values: AddGoalSchema) => {
-    console.log(values)
+    createGoalMutation.mutate(values)
   }
 
   return (
@@ -35,66 +43,35 @@ export default function CreateGoal() {
       </div>
 
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(handleGoalCreate)} className='grid md:grid-cols-2 gap-4'>
-          <FormField
-            control={form.control}
-            name='category'
-            render={() => (
-              <FormItem>
-                <FormLabel>Category</FormLabel>
-                <FormControl>
-                  <Select
-                    value={goalCategory}
-                    onValueChange={(value) => {
-                      setGoalCategory(value as GoalCategories)
-                    }}
-                  >
-                    <SelectTrigger>
-                      <SelectValue />
-                    </SelectTrigger>
-                    <SelectContent>
-                      {GOAL_CATEGORIES.map((category) => (
-                        <SelectItem key={category.id} value={category.id}>
-                          {category.label}
-                        </SelectItem>
-                      ))}
-                    </SelectContent>
-                  </Select>
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          <FormField
-            control={form.control}
-            name='calculationField'
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Calculation Field</FormLabel>
-                <FormControl>
-                  <Select {...field} onValueChange={field.onChange}>
-                    <SelectTrigger>
-                      <SelectValue placeholder='Select a calculation field!' />
-                    </SelectTrigger>
-
-                    <SelectContent className='max-h-72'>
-                      {CATEGORY_CALCULATION_MAP[goalCategory].map((calculation) => (
-                        <SelectItem key={calculation.id} value={calculation.id}>
-                          {calculation.label}
-                        </SelectItem>
-                      ))}
-                    </SelectContent>
-                  </Select>
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
+        <form onSubmit={form.handleSubmit(handleGoalCreate)} className='grid md:grid-cols-2 gap-4 items-center'>
           <InputFieldsRenderer control={form.control} inputs={GOAL_INPUTS} />
 
-          <Button type='submit' className='col-span-full w-max'>
+          <FormField
+            control={form.control}
+            name='target_contribution_source'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Target Contribution Source</FormLabel>
+                <FormControl>
+                  <Select value={field.value?.toString()} onValueChange={field.onChange}>
+                    <SelectTrigger>
+                      <SelectValue placeholder='Target Contribution Source' />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {incomeExpensesQuery?.data?.map((item) => (
+                        <SelectItem key={item.id} value={item.id.toString()}>
+                          {item.name}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button type='submit' className='col-span-full w-max' loading={createGoalMutation.isLoading}>
             <PlusCircle className='w-4 h-4 mr-2' />
             <span>Create Goal</span>
           </Button>

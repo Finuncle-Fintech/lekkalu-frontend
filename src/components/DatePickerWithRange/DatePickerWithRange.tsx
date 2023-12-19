7c15ff97d87d9e800345fff77f02bbc43116d36f
= <rohitbhatwara1@gmail.com>
2023-12-19 22:15:50 +0530

    feat(goal-timeline): filtered timeline using date range

---
+++ src/components/DatePickerWithRange/DatePickerWithRange.tsx
@@ -0,0 +1,58 @@
+import * as React from 'react'
+import { format } from 'date-fns'
+import { Calendar as CalendarIcon } from 'lucide-react'
+import { DateRange } from 'react-day-picker'
+
+import { Button } from '@/components/ui/button'
+import { Calendar } from '@/components/ui/calendar'
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
+import { cn } from '@/utils/utils'
+
+type DatePickerWithRangeProps = Omit<React.HTMLAttributes<HTMLDivElement>, 'onChange'> & {
+  dateRange?: DateRange
+  onChange?: (dateRange: DateRange | undefined) => void
+}
+
+export default function DatePickerWithRange({ className, dateRange, onChange, ...props }: DatePickerWithRangeProps) {
+  const [date, setDate] = React.useState<DateRange | undefined>(dateRange)
+
+  return (
+    <div className={cn('grid gap-2', className)} {...props}>
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button
+            id='date'
+            variant={'outline'}
+            className={cn('w-[300px] justify-start text-left font-normal', !date && 'text-muted-foreground')}
+          >
+            <CalendarIcon className='mr-2 h-4 w-4' />
+            {date?.from ? (
+              date.to ? (
+                <>
+                  {format(date.from, 'LLL dd, y')} - {format(date.to, 'LLL dd, y')}
+                </>
+              ) : (
+                format(date.from, 'LLL dd, y')
+              )
+            ) : (
+              <span>Pick a date</span>
+            )}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className='w-auto p-0' align='start'>
+          <Calendar
+            initialFocus
+            mode='range'
+            defaultMonth={date?.from}
+            selected={date}
+            onSelect={(value) => {
+              setDate(value)
+              onChange?.(value)
+            }}
+            numberOfMonths={2}
+          />
+        </PopoverContent>
+      </Popover>
+    </div>
+  )
+}
--- src/pages/GoalDetails/components/GoalTimeline.tsx
+++ src/pages/GoalDetails/components/GoalTimeline.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useMemo, useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import { Bar, BarChart, Cell, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
 import colors from 'tailwindcss/colors'
 import dayjs from 'dayjs'
+import { DateRange } from 'react-day-picker'
 import { GOALS } from '@/utils/query-keys'
 import { fetchGoalTimeline } from '@/queries/goals'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { cn } from '@/utils/utils'
+import DatePickerWithRange from '@/components/DatePickerWithRange'
 
 type GoalTimelineProps = {
   className?: string
@@ -15,19 +17,43 @@ type GoalTimelineProps = {
 }
 
 export default function GoalTimeline({ className, style, goalId }: GoalTimelineProps) {
+  const [dateRange, setDateRange] = useState<DateRange | undefined>({
+    from: new Date(),
+    to: dayjs().add(1, 'month').toDate(),
+  })
+
   const timelineQuery = useQuery([GOALS.TIMELINE, goalId], () => fetchGoalTimeline(goalId))
+
+  const dataToRender = useMemo(() => {
+    if (!timelineQuery.data) {
+      return []
+    }
+
+    return timelineQuery.data.filter(
+      (item) => dayjs(item.time).isAfter(dateRange?.from) && dayjs(item.time).isBefore(dateRange?.to),
+    )
+  }, [dateRange?.from, dateRange?.to, timelineQuery.data])
 
   return (
     <Card className={cn('h-96 shadow-sm', className)} style={style}>
-      <CardHeader>
+      <CardHeader className='flex justify-between flex-row'>
         <CardTitle>Timeline</CardTitle>
+
+        <DatePickerWithRange
+          dateRange={dateRange}
+          onChange={(range) => {
+            setDateRange(range)
+          }}
+        />
       </CardHeader>
 
       <CardContent className='w-full h-full'>
         <ResponsiveContainer width='100%' height='75%'>
-          <BarChart data={timelineQuery?.data ?? []}>
+          <BarChart data={dataToRender}>
             <Bar dataKey='kpi_value' fill={colors.indigo['500']} background>
-              {timelineQuery.data?.map((_, index) => <Cell key={`cell-${index}`} radius={10} />)}
+              {dataToRender.map((_, index) => (
+                <Cell key={`cell-${index}`} radius={10} />
+              ))}
             </Bar>
             <XAxis
               dataKey='time'
@@ -36,7 +62,7 @@ export default function GoalTimeline({ className, style, goalId }: GoalTimelineP
               tickFormatter={(item) => dayjs(item).format('MMM YY')}
             />
             <YAxis tickLine={false} axisLine={false} />
-            <Tooltip />
+            <Tooltip labelFormatter={(date) => dayjs(date).format('DD-MMM-YYYY')} />
           </BarChart>
         </ResponsiveContainer>
       </CardContent>

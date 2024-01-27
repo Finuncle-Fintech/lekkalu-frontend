0b0d2ceaf70f7389c206276c8eca544319edb257
Lalith <lalith@Nanis-MacBook-Pro.local>
2024-01-27 17:34:44 +0530

    Update user profile details functionality

--- src/schema/user.ts
+++ src/schema/user.ts
@@ -4,6 +4,10 @@ export const userProfileSchema = z.object({
   first_name: z.string().min(3, 'Please enter at least 3 characters!').max(20, 'Please enter at most 20 characters!'),
   last_name: z.string().min(3, 'Please enter at least 3 characters!').max(20, 'Please enter at most 20 characters!'),
   email: z.string().email('Invalid Email!'),
+  username: z
+    .string({ required_error: 'Username is required!' })
+    .min(6, 'Enter at least 6 characters!')
+    .max(20, 'Enter at most 30 characters!'),
 })
 
 export const changePasswordSchema = z

b603446fa9a2b9fbc8de4503b12f4dd5f71ca5ab
Sandip Satyal <sandipsatyal25@gmail.com>
2024-02-09 22:20:41 +0545

    add: feature to view goal timeline in day, week, month and year.

--- src/pages/GoalDetails/components/GoalTimeline.tsx
+++ src/pages/GoalDetails/components/GoalTimeline.tsx
@@ -1,7 +1,6 @@
 import React, { useMemo, useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
-import { Bar, BarChart, Cell, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
-import colors from 'tailwindcss/colors'
+import { CartesianGrid, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
 import dayjs from 'dayjs'
 import { DateRange } from 'react-day-picker'
 import { GOALS } from '@/utils/query-keys'
@@ -9,6 +8,9 @@ import { fetchGoalTimeline } from '@/queries/goals'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { cn } from '@/utils/utils'
 import DatePickerWithRange from '@/components/DatePickerWithRange'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
+import { getDataByMonth, getDataByWeek, getDataByYear } from '../utils/dateTime'
+import { Timeline } from '@/types/goals'
 
 type GoalTimelineProps = {
   className?: string
@@ -16,11 +18,34 @@ type GoalTimelineProps = {
   goalId: number
 }
 
+const dropdownOptions = [
+  {
+    name: 'Day',
+    value: 'day',
+  },
+  {
+    name: 'Week',
+    value: 'week',
+  },
+  {
+    name: 'Month',
+    value: 'month',
+  },
+  {
+    name: 'Year',
+    value: 'year',
+  },
+]
+
+type ChartView = 'day' | 'week' | 'month' | 'year'
+
 export default function GoalTimeline({ className, style, goalId }: GoalTimelineProps) {
   const [dateRange, setDateRange] = useState<DateRange | undefined>({
     from: new Date(),
     to: dayjs().add(1, 'month').toDate(),
   })
+
+  const [viewAs, setViewAs] = useState<ChartView>('day')
 
   const timelineQuery = useQuery([GOALS.TIMELINE, goalId], () => fetchGoalTimeline(goalId))
 
@@ -29,41 +54,67 @@ export default function GoalTimeline({ className, style, goalId }: GoalTimelineP
       return []
     }
 
-    return timelineQuery.data.filter(
+    const validDates = timelineQuery.data.filter(
       (item) => dayjs(item.time).isAfter(dateRange?.from) && dayjs(item.time).isBefore(dateRange?.to),
     )
-  }, [dateRange?.from, dateRange?.to, timelineQuery.data])
+
+    return aggregatedDates(validDates)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [dateRange?.from, dateRange?.to, timelineQuery.data, viewAs])
+
+  function aggregatedDates(dates: Timeline[]) {
+    switch (viewAs) {
+      case 'day':
+        return dates
+      case 'month':
+        return getDataByMonth(dates)
+      case 'week':
+        return getDataByWeek(dates)
+      case 'year':
+        return getDataByYear(dates)
+      default:
+        return dates
+    }
+  }
 
   return (
     <Card className={cn('h-96 shadow-sm', className)} style={style}>
-      <CardHeader className='flex justify-between flex-row'>
+      <CardHeader className='flex flex-start flex-row'>
         <CardTitle>Timeline</CardTitle>
+        <div className='flex ml-auto gap-5'>
+          <DatePickerWithRange
+            dateRange={dateRange}
+            onChange={(range) => {
+              setDateRange(range)
+            }}
+          />
 
-        <DatePickerWithRange
-          dateRange={dateRange}
-          onChange={(range) => {
-            setDateRange(range)
-          }}
-        />
+          <Select onValueChange={(value: ChartView) => setViewAs(value)} value={viewAs}>
+            <SelectTrigger className='text-xs focus:ring-transparent w-[100px]' placeholder='View as'>
+              <span className='flex items-center'>
+                <SelectValue placeholder='Select' />
+              </span>
+            </SelectTrigger>
+            <SelectContent className='max-h-60'>
+              {dropdownOptions.map(({ name, value }) => (
+                <SelectItem value={value} key={`${name}_${value}`} className='text-xs'>
+                  {name}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </div>
       </CardHeader>
 
       <CardContent className='w-full h-full'>
         <ResponsiveContainer width='100%' height='75%'>
-          <BarChart data={dataToRender}>
-            <Bar dataKey='kpi_value' fill={colors.indigo['500']} background>
-              {dataToRender.map((_, index) => (
-                <Cell key={`cell-${index}`} radius={10} />
-              ))}
-            </Bar>
-            <XAxis
-              dataKey='time'
-              tickLine={false}
-              axisLine={false}
-              tickFormatter={(item) => dayjs(item).format('MMM YY')}
-            />
-            <YAxis tickLine={false} axisLine={false} />
-            <Tooltip labelFormatter={(date) => dayjs(date).format('DD-MMM-YYYY')} />
-          </BarChart>
+          <LineChart data={dataToRender} width={730} height={250} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+            <CartesianGrid strokeDasharray='3 1' />
+            <XAxis dataKey='time' tickFormatter={(date) => dayjs(date).format('DD MMM YYYY')} />
+            <YAxis dataKey={'kpi_value'} />
+            <Tooltip labelFormatter={(date) => dayjs(date).format('DD MMM YYYY')} />
+            <Line dataKey={'kpi_value'} name='Kpi Value' />
+          </LineChart>
         </ResponsiveContainer>
       </CardContent>
     </Card>

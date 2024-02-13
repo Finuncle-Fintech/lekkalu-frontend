0f754da65c7ef3954368ad7c1b179f0f9ffd5ff7
Sandip Satyal <sandipsatyal25@gmail.com>
2024-02-13 22:47:44 +0545

    add: custom error message for contribution_source field

--- src/schema/goals.ts
+++ src/schema/goals.ts
@@ -1,11 +1,21 @@
+import dayjs from 'dayjs'
 import z from 'zod'
+
+const customErrorMap: z.ZodErrorMap = (issue, ctx) => {
+  if (issue.code === z.ZodIssueCode.invalid_type) {
+    if (issue.expected === 'number') {
+      return { message: 'Required' }
+    }
+  }
+  return { message: ctx.defaultError }
+}
 
 export const addGoalSchema = z.object({
   name: z.string(),
   target_value: z.number().min(1).max(100),
-  target_contribution_source: z.coerce.number(),
+  target_contribution_source: z.coerce.number({ errorMap: customErrorMap }),
   track_kpi: z.string(),
-  target_date: z.string(),
+  target_date: z.string().transform((value) => dayjs(value).format('YYYY-MM-DD')),
   goal_proportionality: z.string(),
 })
 

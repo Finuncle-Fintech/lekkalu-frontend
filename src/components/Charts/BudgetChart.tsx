15a27f1f957731ffe98e2938a0e2075e3b2f5eee
Bharath Kumar Chowdary Gundala <bharathkumarchowdary006@gmail.com>
2024-02-20 03:12:39 +0530

    fixes budget's filter problem fixes rounding of spent and exceed amounts in budgets

--- src/components/Charts/BudgetChart.tsx
+++ src/components/Charts/BudgetChart.tsx
@@ -16,15 +16,15 @@ import { useUserPreferences } from '@/hooks/use-user-preferences'
 const dropdownOptions = [
   {
     name: 'Last 3 Months',
-    value: 'month-3',
+    value: '3',
   },
   {
     name: 'Last 6 Months',
-    value: 'month-6',
+    value: '6',
   },
   {
     name: 'Last Year',
-    value: 'year',
+    value: '12',
   },
 ]
 
@@ -40,7 +40,7 @@ export default function BudgetChart() {
   })
 
   const { data, isLoading } = useQuery([EXPENSES.MONTHLY_EXPENSES], fetchMonthlyExpenses)
-  const [timeRange, setTimeRange] = useState('month-3')
+  const [timeRange, setTimeRange] = useState('3')
   const monthlyData = useMemo(() => {
     if (!data || !budgetData) {
       return []
@@ -48,27 +48,34 @@ export default function BudgetChart() {
 
     return data
       ?.map((ele) => ({ ...ele, budget: budgetData?.find((item) => item.month === ele.month)?.limit ?? 0 }))
-      .sort((a, b) => b.month - a.month)
-  }, [data, budgetData])
+      .sort((a, b) => {
+        if (a.year !== b.year) {
+          return b.year - a.year
+        } else {
+          return b.month - a.month
+        }
+      })
+      .slice(0, parseInt(timeRange))
+  }, [data, budgetData, timeRange])
 
   if (isLoading) {
-    return <div className='w-full animate-pulse bg-gray-300 h-96 rounded-md' />
+    return <div className="w-full animate-pulse bg-gray-300 h-96 rounded-md" />
   }
 
   return (
-    <Card className='shadow-sm p-2'>
-      <CardHeader className='flex justify-between w-full flex-row'>
+    <Card className="shadow-sm p-2">
+      <CardHeader className="flex justify-between w-full flex-row">
         <div>
-          <CardTitle>Expenses Summary</CardTitle>
+          <CardTitle>Budgets</CardTitle>
           <CardDescription>Summary of your expenses against your budgets</CardDescription>
         </div>
 
         <Select onValueChange={(str) => setTimeRange(str)} value={timeRange}>
-          <SelectTrigger className='w-52'>
-            <CalendarDays className='w-4 h-4' />
-            <SelectValue placeholder='Select' />
+          <SelectTrigger className="w-52">
+            <CalendarDays className="w-4 h-4" />
+            <SelectValue placeholder="Select" />
           </SelectTrigger>
-          <SelectContent className='max-h-60'>
+          <SelectContent className="max-h-60">
             {dropdownOptions.map(({ name, value }) => (
               <SelectItem value={value} key={`${name}_${value}`}>
                 {name}
@@ -82,9 +89,9 @@ export default function BudgetChart() {
         {monthlyData && monthlyData.length > 0 ? (
           monthlyData?.map((ele, key) => <MonthlyProgressBar data={ele} key={key} />)
         ) : (
-          <div className='w-full h-full flex items-center justify-center flex-col'>
-            <BoxIcon className='w-8 h-8 text-muted-foreground' />
-            <h3 className='text-muted-foreground'>No Expenses found</h3>
+          <div className="w-full h-full flex items-center justify-center flex-col">
+            <BoxIcon className="w-8 h-8 text-muted-foreground" />
+            <h3 className="text-muted-foreground">No Budgets found</h3>
           </div>
         )}
       </CardContent>
@@ -92,9 +99,7 @@ export default function BudgetChart() {
   )
 }
 
-function MonthlyProgressBar({
-  data,
-}: {
+function MonthlyProgressBar({ data }: {
   data: {
     budget: number
     year: number
@@ -108,11 +113,11 @@ function MonthlyProgressBar({
   const currency = preferences.currencyUnit
   const { budget, month, spent, year } = data
   const percentage = round((spent / budget) * 100, 2)
-  const leftAmount = budget - spent
+  const leftAmount = round(budget - spent, 2)
 
   return (
-    <div className='flex justify-between items-center text-center gap-2'>
-      <div className='text-xs font-semibold w-16 text-right'>
+    <div className="flex justify-between items-center text-center gap-2">
+      <div className="text-xs font-semibold w-16 text-right">
         {dayjs()
           .month(month - 1)
           .format('MMM')}{' '}
@@ -122,32 +127,26 @@ function MonthlyProgressBar({
       <TooltipProvider>
         <Tooltip>
           <TooltipTrigger asChild>
-            <div className='flex-1 cursor-pointer'>
-              <div className='flex justify-between items-center'>
-                <h6 className='text-xs font-medium uppercase text-gray-700'>spent {spent}</h6>
-                <h6
-                  className={cn(
-                    'text-xs font-medium uppercase pb-1',
-                    percentage < 80 ? 'text-green-500' : percentage < 95 ? 'text-amber-500' : 'text-red-500',
-                  )}
-                >
-                  {budget
-                    ? leftAmount < 0
-                      ? `Exceeded ${Math.abs(leftAmount)}`
-                      : `left ${leftAmount}`
-                    : 'budget not set'}
+            <div className="flex-1 cursor-pointer">
+              <div className="flex justify-between items-center">
+                <h6 className="text-xs font-medium uppercase text-gray-700">spent {spent.toFixed(2)}</h6>
+                <h6 className={cn(
+                  'text-xs font-medium uppercase pb-1',
+                  percentage < 80 ? 'text-green-500' : percentage < 95 ? 'text-amber-500' : 'text-red-500',
+                )}>
+                  {budget ? leftAmount < 0 ? `Exceeded ${Math.abs(leftAmount)}` : `left ${leftAmount}` : 'budget not set'}
                 </h6>
               </div>
 
               <Progress
                 value={percentage}
-                className='h-[10px]  mb-2 bg-slate-100'
+                className="h-[10px]  mb-2 bg-slate-100"
                 indicatorClassName={percentage < 80 ? 'bg-green-400' : percentage < 95 ? 'bg-amber-400' : 'bg-red-400'}
               />
             </div>
           </TooltipTrigger>
           {budget ? (
-            <TooltipContent className='text-left'>
+            <TooltipContent className="text-left">
               <div>
                 Budget : {currency}
                 {budget}

dc34d8693b69d5cd39ba9bbcecaf777f0d9cc26b
Sandip Satyal <sandipsatyal25@gmail.com>
2024-03-10 21:25:42 +0545

    add: display comparisons in list page.

--- src/pages/Scenarios/index.tsx
+++ src/pages/Scenarios/index.tsx
@@ -6,12 +6,24 @@ import { range } from 'lodash'
 import Page from '@/components/Page/Page'
 import { buttonVariants } from '@/components/ui/button'
 import { Skeleton } from '@/components/ui/skeleton'
-import Scenario from './components/Scenario'
+import Comparison from './components/Comparison'
+
+const comparisons = [
+  { name: 'Comparison 1', uid: Math.random(), access: 'private', scenarios: [1, 2, 3, 4] },
+  { name: 'This is another comparison', uid: Math.random(), access: 'public', scenarios: [1, 2, 3, 4] },
+  { name: 'World is under fire, run bro run.', uid: Math.random(), access: 'private', scenarios: [1, 2, 3, 4] },
+  {
+    name: 'Jcole is better than kendrick and drake combined, come at me.',
+    uid: Math.random(),
+    access: 'public',
+    scenarios: [1, 2, 3, 4],
+  },
+]
 
 export default function Scenarios() {
   const IS_LOADING = false
   return (
-    <Page>
+    <Page className='space-y-4'>
       <div className='flex justify-end'>
         <Link to='/scenarios/new' className={buttonVariants({ variant: 'default' })}>
           <PlusIcon className='w-4 h-4 mr-2' />
@@ -26,8 +38,10 @@ export default function Scenarios() {
             ))}
           </div>
         ) : (
-          <div>
-            <Scenario />
+          <div className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-10'>
+            {comparisons?.map((each) => (
+              <Comparison key={each?.name} id={each?.uid} name={each?.name} access={each?.access} />
+            ))}
           </div>
         )}
       </div>

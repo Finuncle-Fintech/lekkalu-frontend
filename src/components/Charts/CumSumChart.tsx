cde9411eb2157b8423d0b79d621c2bee6e1568f1
Hardik Desai <hp676913@gmail.com>
2024-03-16 13:58:29 +0530

    feat: :sparkles: redesign exisiting charts with apexcharts

--- src/components/Charts/CumSumChart.tsx
+++ src/components/Charts/CumSumChart.tsx
@@ -1,38 +1,12 @@
 import React, { useMemo } from 'react'
-import {
-  ResponsiveContainer,
-  TooltipProps,
-  ComposedChart,
-  Line,
-  Bar,
-  CartesianGrid,
-  XAxis,
-  YAxis,
-  Tooltip,
-  Legend,
-} from 'recharts'
-import { ValueType, NameType } from 'recharts/types/component/DefaultTooltipContent'
 import { useQuery } from '@tanstack/react-query'
+import Chart from 'react-apexcharts'
 import dayjs from 'dayjs'
 import colors from 'tailwindcss/colors'
 import { useUserPreferences } from '@/hooks/use-user-preferences'
 import { fetchMonthlyExpenses } from '@/queries/expense'
 import { EXPENSES } from '@/utils/query-keys'
-
-const CustomTooltip = ({ active, payload, label }: TooltipProps<ValueType, NameType>) => {
-  const { preferences } = useUserPreferences()
-
-  if (active && payload && payload.length) {
-    return (
-      <div className='border bg-white p-1'>
-        <p className='m-1'>{label}</p>
-        <p className='m-1 text-green-500 '>{`${payload[0].name} : ${preferences.currencyUnit} ${payload[0].value}`}</p>
-      </div>
-    )
-  }
-
-  return null
-}
+import { formatIndianMoneyNotation } from '@/utils/format-money'
 
 export const CumSumChart = () => {
   const { data, isLoading } = useQuery([EXPENSES.MONTHLY_EXPENSES], fetchMonthlyExpenses)
@@ -44,49 +18,70 @@ export const CumSumChart = () => {
       return []
     }
 
-    return data.map((monthlyExpense) => ({
-      ...monthlyExpense,
-      month: dayjs().month(monthlyExpense.month).format('MMMM'),
-    }))
+    return data
+      .map((monthlyExpense) => ({
+        ...monthlyExpense,
+        month: dayjs().month(monthlyExpense.month).format('MMMM'),
+      }))
+      .sort((a, b) => a.year + b.year)
   }, [data])
 
+  const chartOptions: ApexCharts.ApexOptions = {
+    chart: {
+      height: 350,
+      type: 'bar',
+      toolbar: {
+        show: true,
+      },
+      foreColor: '#000',
+      dropShadow: {
+        enabled: true,
+        color: '#000',
+        top: 5,
+        left: 1,
+        blur: 5,
+        opacity: 0.18,
+      },
+    },
+    stroke: {
+      curve: 'smooth',
+    },
+    colors: [colors.green['500']],
+    dataLabels: {
+      enabled: false,
+    },
+    plotOptions: {
+      bar: {
+        borderRadius: 10,
+        columnWidth: '60%',
+      },
+    },
+    xaxis: {
+      categories: cumSumData.map((item) => item.month.slice(0, 3)),
+      labels: {
+        formatter: (value) => value,
+      },
+    },
+    yaxis: {
+      labels: {
+        formatter: (value) => `${preferences.currencyUnit} ${formatIndianMoneyNotation(value, 1)}`,
+      },
+    },
+  }
+  const chartSeries: ApexAxisChartSeries | ApexNonAxisChartSeries = [
+    {
+      name: 'Cum Sum',
+      data: cumSumData.map((item) => item.cum_sum),
+    },
+  ]
   if (isLoading) {
     return <div className='w-full animate-pulse bg-gray-300 h-96' />
   }
 
   return (
     <div className='border p-4 w-full'>
-      <h3 className='text-lg text-center'>Monthly Cum Sum</h3>
-
-      <ResponsiveContainer width='100%' aspect={2}>
-        <ComposedChart
-          data={cumSumData}
-          margin={{
-            top: 20,
-            right: 20,
-            bottom: 25,
-            left: 25,
-          }}
-        >
-          <CartesianGrid strokeDasharray='0 0' vertical={false} />
-          <XAxis dataKey='month' dy={10} />
-          <YAxis
-            tickFormatter={(tick) => {
-              return `${preferences.currencyUnit} ${tick}`
-            }}
-          />
-          <Tooltip content={<CustomTooltip />} />
-          <Legend
-            layout='horizontal'
-            align='center'
-            wrapperStyle={{
-              position: 'relative',
-            }}
-          />
-          <Bar dataKey='cum_sum' barSize={160} fill={colors.green['500']} />
-          <Line type='monotone' dataKey='cum_sum' stroke={colors.green['500']} strokeWidth={3} />
-        </ComposedChart>
-      </ResponsiveContainer>
+      <h3 className='text-lg text-center'>Cumulative Surplus Deficit</h3>
+      <Chart options={chartOptions} series={chartSeries} type='bar' height={320} />
     </div>
   )
 }

03417bebd1942c50aa6e8a6951840a109ed7c51a
HitDoshi <hitdoshi2001@gmail.com>
2024-03-17 13:36:18 +0530

    convert email verification screen to modal & add remind me later feature

--- src/components/AppShell/components/MobileMenu.tsx
+++ src/components/AppShell/components/MobileMenu.tsx
@@ -60,42 +60,27 @@ export default function MobileMenu({ isUnAuthenticatedHeader }: { isUnAuthentica
 
             {isUnAuthenticatedHeader ? (
               <When
-                truthy={Boolean(tokenData) && Boolean(userData?.email_verified)}
+                truthy={Boolean(tokenData)}
                 fallback={
-                  <When
-                    truthy={Boolean(tokenData) && Boolean(!userData?.email_verified)}
-                    fallback={
-                      <div className='space-x-2 flex w-full'>
-                        <Link
-                          to='/signin'
-                          className={cn(
-                            buttonVariants({ variant: 'outline' }),
-                            'border-1 border border-black/50',
-                            'w-full',
-                          )}
-                        >
-                          Signin
-                        </Link>
-
-                        <Link
-                          to='/signup'
-                          className={cn(
-                            buttonVariants({ variant: 'secondary' }),
-                            'w-full border-1 border border-black/50',
-                          )}
-                        >
-                          Signup
-                        </Link>
-                      </div>
-                    }
-                  >
+                  <div className='space-x-2 flex w-full'>
                     <Link
-                      to='/email-verification'
+                      to='/signin'
+                      className={cn(
+                        buttonVariants({ variant: 'outline' }),
+                        'border-1 border border-black/50',
+                        'w-full',
+                      )}
+                    >
+                      Signin
+                    </Link>
+
+                    <Link
+                      to='/signup'
                       className={cn(buttonVariants({ variant: 'secondary' }), 'w-full border-1 border border-black/50')}
                     >
-                      Verify Email
+                      Signup
                     </Link>
-                  </When>
+                  </div>
                 }
               >
                 <Link to='/dashboard' className={cn(buttonVariants({ variant: 'secondary' }), 'w-full')}>

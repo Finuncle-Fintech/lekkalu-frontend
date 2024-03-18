56b973e48240623378fdddaccf7e89db16a42fa1
HitDoshi <hitdoshi2001@gmail.com>
2024-03-18 09:02:05 +0530

    Fix ESLint issue

--- src/pages/EmailVerification/EmailVerification.tsx
+++ src/pages/EmailVerification/EmailVerification.tsx
@@ -48,7 +48,7 @@ export const EmailVerification = ({ isEmailVerifiedDialogOpen, setIsEmailVerifie
 
   return (
     <Dialog open={isEmailVerifiedDialogOpen} onOpenChange={setIsEmailVerifiedDialogOpen}>
-      <DialogContent className='m-4'>        
+      <DialogContent className='m-4'>
         <DialogHeader>
           <DialogTitle>Verify Email</DialogTitle>
         </DialogHeader>

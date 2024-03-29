8a5576b15ae11513ec42e20286151347faf3c3d5
Sandip Satyal <sandipsatyal25@gmail.com>
2024-03-29 12:54:56 +0545

    add: create goal on scenario creation.

--- src/schema/comparisons.ts
+++ src/schema/comparisons.ts
@@ -2,8 +2,8 @@ import z from 'zod'
 
 export const addComparisonSchema = z.object({
   name: z.string(),
-  access: z.enum(['Private', 'Public']),
-  scenarios: z.array(z.number()),
+  access: z.enum(['Private', 'Public']).optional(),
+  scenarios: z.array(z.number()).optional(),
 })
 
 export type AddComaprisonSchema = z.infer<typeof addComparisonSchema>

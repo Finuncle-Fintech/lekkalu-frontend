067a7029df6e68c21c76b7c06763026c7d2df890
Sandip Satyal <sandipsatyal25@gmail.com>
2024-03-29 12:54:56 +0545

    add: create goal on scenario creation.

--- src/pages/Scenarios/AddScenario.tsx
+++ src/pages/Scenarios/AddScenario.tsx
@@ -3,12 +3,19 @@ import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useNavigate } from 'react-router-dom'
 import { useMutation } from '@tanstack/react-query'
+import dayjs from 'dayjs'
+import { AxiosInstance } from 'axios'
 import Page from '@/components/Page/Page'
 import PageTitle from '@/components/DetailPageHeading'
 import { AddScenarioSchemas, addScenarioSchemas } from '@/schema/scenarios'
 import ScenarioForm from './components/Form'
 import { createScenarios } from '@/queries/scenarios'
 import { toast } from '@/components/ui/use-toast'
+import { useImaginaryAuth } from './context/use-imaginaryAuth'
+import { AddGoalSchema } from '@/schema/goals'
+import { loginImaginaryUser } from '@/queries/auth'
+
+const ONE_YEAR_LATER = dayjs().add(1, 'year').toISOString().split('T')[0]
 
 const AddScenario = () => {
   const navigate = useNavigate()
@@ -16,10 +23,46 @@ const AddScenario = () => {
     resolver: zodResolver(addScenarioSchemas),
   })
 
+  const { getAPIClientForImaginaryUser } = useImaginaryAuth()
+
+  async function addGoal(dto: AddGoalSchema, apiClient: AxiosInstance) {
+    const { data } = await apiClient.post('financial_goal/', dto)
+    return data
+  }
+
+  const { mutate: login } = useMutation(loginImaginaryUser)
+  const { mutate: createGoalForImaginaryUser } = useMutation((apiClient: AxiosInstance) =>
+    addGoal(
+      {
+        name: `${Math.random()}-goal`,
+        goal_proportionality: 'HigherTheBetter',
+        track_kpi: 'NetWorth',
+        target_value: 1,
+        target_date: ONE_YEAR_LATER,
+      },
+      apiClient,
+    ),
+  )
+
   const { mutate: addScenario, isLoading } = useMutation(createScenarios, {
     onSuccess: (data) => {
       toast({ title: 'Scenario created successfully!' })
-      navigate(`/scenarios/${data.id}`)
+      login(
+        { username: data.imag_username, password: data.imag_password, id: data.id },
+        {
+          onSuccess: (logginInfo) => {
+            const apiClient = getAPIClientForImaginaryUser(logginInfo.access, 'v2')
+            createGoalForImaginaryUser(apiClient, {
+              onSuccess: () => {
+                navigate(`/scenarios/${data.id}`)
+              },
+              onError: (e) => {
+                console.log('error occured', e)
+              },
+            })
+          },
+        },
+      )
     },
     onError: (response: any) => {
       const message = response?.response?.data?.message

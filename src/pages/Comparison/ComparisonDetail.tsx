2de8a147beeebfd2be00f49767fece382c8a861e
Sandip Satyal <sandipsatyal25@gmail.com>
2024-03-30 14:04:44 +0545

    add: simulate graph in comparison detail page.

--- src/pages/Comparison/ComparisonDetail.tsx
+++ src/pages/Comparison/ComparisonDetail.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { CartesianGrid, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
 import { useMutation, useQuery } from '@tanstack/react-query'
@@ -15,12 +15,19 @@ import { editComparisons, fetchComparisonsById } from '@/queries/comparisons'
 import { fetchScenarios } from '@/queries/scenarios'
 import { type Scenario as ScenarioType } from '@/types/scenarios'
 import { Comparison } from '@/types/comparison'
-import { queryClient } from '@/utils/client'
+import { queryClient, tokenClient } from '@/utils/client'
+import { Button } from '@/components/ui/button'
+import { Goal, Timeline } from '@/types/goals'
+import { useImaginaryAuth } from '../Scenarios/context/use-imaginaryAuth'
+import { generateRandomColor, mergeArraysByDate } from './utils/dateTime'
 
 const ComparisonDetail = () => {
   const comparisonId = useParams().id
+  const { getAPIClientForImaginaryUser } = useImaginaryAuth()
 
   const [selectedScenarios, setSelectedScenarios] = useState<Array<number>>([])
+  const [timelineData, setTimelineData] = useState<any>()
+  const [calculatedTimelineDate, setCalculatedTimelineData] = useState<any>()
 
   const handleScenarioSelect = (id: number) => {
     const _selectedScenarios = [...selectedScenarios]
@@ -32,7 +39,7 @@ const ComparisonDetail = () => {
     setSelectedScenarios(_selectedScenarios)
   }
 
-  const { data: comparison, refetch: refetchComparison } = useQuery([`${COMPARISON.COMPARISON}-${comparisonId}`], () =>
+  const { data: comparison } = useQuery([`${COMPARISON.COMPARISON}-${comparisonId}`], () =>
     fetchComparisonsById(Number(comparisonId)),
   )
 
@@ -53,6 +60,39 @@ const ComparisonDetail = () => {
   const scenariosForThisComparison = scenarios?.filter((each: ScenarioType) => comparison?.scenarios.includes(each?.id))
   const remaningScenarios = scenarios?.filter((each) => !comparison?.scenarios.includes(each?.id))
 
+  const timelineDataAPICall = async (dto: { username: string; password: string; scenarioName: string }) => {
+    const loginResponse = await tokenClient.post<{ access: string; refresh: string }>('/', dto)
+    const apiClient = getAPIClientForImaginaryUser(loginResponse.data.access, 'v2')
+    const goalData = await apiClient.get<Goal[]>('financial_goal/')
+    const timelineData = await apiClient.get<Timeline[]>(`financial_goal/timeline/${goalData?.data[0]?.id}`)
+    return { [dto.scenarioName]: timelineData.data }
+  }
+
+  const {
+    mutate: login,
+    isSuccess,
+    isLoading,
+  } = useMutation(
+    async ({ password, username, scenarioName }: any) => {
+      const results = await timelineDataAPICall({ password, username, scenarioName })
+      return results
+    },
+    {
+      onSuccess: (data) => {
+        setTimelineData((prevData: any) => {
+          return { ...prevData, ...data }
+        })
+      },
+    },
+  )
+
+  const handleSimulate = () => {
+    setTimelineData({})
+    scenariosForThisComparison?.forEach((each) => {
+      login({ password: each?.imag_password, username: each?.imag_username, scenarioName: each?.name })
+    })
+  }
+
   const handleAddScenariosToComparison = () => {
     const _scenarios = comparison?.scenarios as Array<number>
     const _selectedScenarios = [..._scenarios, ...selectedScenarios]
@@ -68,14 +108,28 @@ const ComparisonDetail = () => {
     scenarioMutationInComparison({ scenarios: remainingScenarios })
   }
 
+  useEffect(() => {
+    if (!isLoading && isSuccess) {
+      const result = mergeArraysByDate(timelineData)
+      setCalculatedTimelineData(result)
+    }
+  }, [isSuccess, isLoading])
+
   return (
     <Page className='space-y-8'>
-      <PageTitle
-        backUrl='/comparisons'
-        backUrlTitle='Back to comparisons'
-        title={comparison?.name || ''}
-        key={comparisonId}
-      />
+      <div className='flex justify-between'>
+        <PageTitle
+          backUrl='/comparisons'
+          backUrlTitle='Back to comparisons'
+          title={comparison?.name || ''}
+          key={comparisonId}
+        />
+        <div>
+          <Button variant={'default'} onClick={handleSimulate} loading={isLoading}>
+            Simulate
+          </Button>
+        </div>
+      </div>
       <h2 className='font-bold'>
         {scenariosForThisComparison?.length
           ? 'List of Scenarios in this comparison.'
@@ -101,42 +155,58 @@ const ComparisonDetail = () => {
         />
       </div>
 
-      <div>
-        <Card className={cn('h-[600px] sm:h-96 pb-20 sm:pb-0 shadow-sm')}>
-          <CardHeader className='flex flex-start flex-col sm:flex-row'>
-            <CardTitle>Graph</CardTitle>
-          </CardHeader>
-          <CardContent className='w-full h-full'>
-            <ResponsiveContainer width='100%' height='75%'>
-              <LineChart
-                data={[
-                  { time: '2022-01-11', scenario1: 16, scenario2: 22, value: 44 },
-                  { time: '2022-02-11', scenario1: 29, scenario2: 22, value: 44 },
-                  { time: '2022-03-11', scenario1: 8, scenario2: 22, value: 44 },
-                  { time: '2022-04-11', scenario1: 39, scenario2: 22, value: 44 },
-                ]}
-                width={730}
-                height={250}
-                margin={{ top: 5, right: 0, left: 10, bottom: 5 }}
-              >
-                <CartesianGrid strokeDasharray='3 1' />
-                <XAxis dataKey='time' tickFormatter={(date) => dayjs(date).format('MMM YYYY')} />
-                <YAxis dataKey={'value'} />
-                <Tooltip labelFormatter={(date) => dayjs(date).format('DD MMM YYYY')} />
-                <Line type='monotone' dataKey={'scenario1'} name='Scenario 1' strokeWidth={1} dot={false} />
-                <Line
-                  type='monotone'
-                  dataKey={'scenario2'}
-                  name='Scenario 2'
-                  stroke='red'
-                  strokeWidth={1}
-                  dot={false}
-                />
-              </LineChart>
-            </ResponsiveContainer>
-          </CardContent>
-        </Card>
-      </div>
+      {timelineData ? (
+        <div>
+          <Card className={cn('h-[600px] sm:h-96 pb-20 sm:pb-0 shadow-sm')}>
+            <CardHeader className='flex flex-start flex-col sm:flex-row'>
+              <CardTitle>Graph</CardTitle>
+            </CardHeader>
+            {
+              <CardContent className='w-full h-full'>
+                {isLoading ? (
+                  <p>Loading...</p>
+                ) : (
+                  <ResponsiveContainer width='100%' height='75%'>
+                    <LineChart
+                      data={calculatedTimelineDate}
+                      width={730}
+                      height={250}
+                      margin={{ top: 5, right: 0, left: 10, bottom: 5 }}
+                    >
+                      <CartesianGrid strokeDasharray='3 1' />
+                      <XAxis
+                        dataKey='time'
+                        tickFormatter={(date) => dayjs(date).format('MMM YYYY')}
+                        allowDataOverflow
+                      />
+                      <YAxis />
+                      <Tooltip labelFormatter={(date) => dayjs(date).format('DD MMM YYYY')} />
+
+                      {isSuccess ? (
+                        Object.keys(timelineData).map((each, index) => (
+                          <Line
+                            key={each}
+                            type='monotone'
+                            dataKey={each}
+                            name={each}
+                            stroke={generateRandomColor(index)}
+                            strokeWidth={1}
+                            dot={false}
+                          />
+                        ))
+                      ) : (
+                        <></>
+                      )}
+                    </LineChart>
+                  </ResponsiveContainer>
+                )}
+              </CardContent>
+            }
+          </Card>
+        </div>
+      ) : (
+        <></>
+      )}
     </Page>
   )
 }

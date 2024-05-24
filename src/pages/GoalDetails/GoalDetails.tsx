b635ae128e86dacee29c381fa49ebfbedc6288d2
Sandip Satyal <sandipsatyal25@gmail.com>
2024-05-24 18:31:47 +0545

    add: allow user to edit goal properties in goal detail page.

--- src/pages/GoalDetails/GoalDetails.tsx
+++ src/pages/GoalDetails/GoalDetails.tsx
@@ -1,22 +1,44 @@
-import React, { useMemo } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import { BadgeCheckIcon, GaugeIcon, SplitIcon, TargetIcon } from 'lucide-react'
 import { useParams } from 'react-router-dom'
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import Page from '@/components/Page/Page'
 import { BALANCE_SHEET, GOALS } from '@/utils/query-keys'
-import { fetchGoalDetails } from '@/queries/goals'
+import { editGoal, fetchGoalDetails, fetchKPITypes } from '@/queries/goals'
 import GoalTimeline from './components/GoalTimeline'
 import BackButton from './components/BackButton'
 import { fetchIncomeExpenses } from '@/queries/income-statement'
 import { convertDays, goalReachedString } from './utils/dateTime'
 import { formatIndianMoneyNotation } from '@/utils/format-money'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
+import { Goal as GoalType } from '@/types/goals'
+import { toast } from '@/components/ui/use-toast'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
 
 export default function GoalDetails() {
   const { id } = useParams() as { id: string }
+  const qc = useQueryClient()
+
+  const [target, setTarget] = useState<number | string>()
+  const [kpi, setKpi] = useState<string>()
+  const [source, setSource] = useState<string>()
+  const [reachableDate, setRechableDate] = useState<number | string>()
+  const [name, setName] = useState<string>()
+
+  const [allowRename, setAllowRename] = useState(false)
+  const [allowEditTarget, setAllowEditTarget] = useState(false)
+  const [allowEditReachableDate, setAllowEditReachableDate] = useState(false)
 
   const { data: incomeExpenses } = useQuery({ queryKey: [BALANCE_SHEET.INCOME_EXPENSES], queryFn: fetchIncomeExpenses })
 
-  const { isLoading, data } = useQuery({
+  const { data: getTargetKpi } = useQuery({ queryKey: [GOALS.KPI_TYPES], queryFn: fetchKPITypes })
+
+  const {
+    data,
+    isLoading,
+    isSuccess: goalDetailFetchedSuccess,
+  } = useQuery({
     queryKey: [GOALS.DETAILS, Number(id)],
     queryFn: () => fetchGoalDetails(Number(id)),
     select: (data) => {
@@ -27,10 +49,59 @@ export default function GoalDetails() {
     },
   })
 
-  const reachableDays = useMemo(
+  useEffect(() => {
+    if (goalDetailFetchedSuccess) {
+      setTarget(data?.target_value)
+      setKpi(data?.track_kpi)
+      setSource(data?.target_contribution_source)
+      setRechableDate(data?.reachable_by_days)
+      setName(data?.name)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [goalDetailFetchedSuccess])
+
+  const goalMutation = useMutation({
+    mutationFn: (dto: Partial<GoalType>) => editGoal(Number(id), dto),
+    onSuccess: () => {
+      toast({ title: 'Goal edited successfully!', variant: 'default' })
+      qc.invalidateQueries({ queryKey: [GOALS.DETAILS, Number(id)] })
+    },
+    onError: () => {
+      setTarget(data?.target_value)
+      setKpi(data?.track_kpi)
+      setSource(data?.target_contribution_source)
+      setRechableDate(data?.reachable_by_days)
+      toast({ title: 'Something went wrong.', variant: 'destructive' })
+    },
+  })
+
+  const reachableDaysString = useMemo(
     () => goalReachedString(convertDays(data?.reachable_by_days || 0)),
     [data?.reachable_by_days],
   )
+
+  const getTargetId = (targetName: string) => {
+    const id = incomeExpenses?.find(({ name }) => targetName === name)?.id
+    if (id === 0) {
+      return undefined
+    }
+    return id
+  }
+
+  const editName = () => {
+    goalMutation.mutate({ name })
+    setAllowRename(false)
+  }
+
+  const editTarget = () => {
+    goalMutation.mutate({ target_value: Number(target) })
+    setAllowEditTarget(false)
+  }
+
+  const editReachableDays = () => {
+    goalMutation.mutate({ reachable_by_days: Number(reachableDate) })
+    setAllowEditReachableDate(false)
+  }
 
   if (isLoading) {
     return (
@@ -54,7 +125,33 @@ export default function GoalDetails() {
 
   return (
     <Page className='space-y-4'>
-      <h1 className='text-2xl font-bold mb-8'>{data.name}</h1>
+      <div>
+        {allowRename ? (
+          <div className='flex w-96 gap-4'>
+            <Input
+              type='text'
+              value={name}
+              onChange={(e) => setName(e?.target?.value)}
+              onKeyDown={(e) => {
+                if (e.code === 'Enter') {
+                  editName()
+                }
+                if (e.code === 'Escape') {
+                  setName(data.name)
+                  setAllowRename(false)
+                }
+              }}
+            />
+            <Button type='button' variant={'ghost'} onClick={editName}>
+              Edit
+            </Button>
+          </div>
+        ) : (
+          <h1 className='text-2xl font-bold mb-8 w-fit' onClick={() => setAllowRename(true)} title='Click to edit'>
+            {name}
+          </h1>
+        )}
+      </div>
       <BackButton />
       <div className='grid md:grid-cols-2 gap-4'>
         <div className='flex'>
@@ -62,7 +159,32 @@ export default function GoalDetails() {
             <TargetIcon className='w-4 h-4' />
             <div>Target</div>
           </div>
-          <div className='flex-1 font-medium'>{formatIndianMoneyNotation(data.target_value)}</div>
+          <div className='flex-1 font-medium self-center'>
+            {allowEditTarget ? (
+              <div className='flex gap-4 w-72'>
+                <Input
+                  type='number'
+                  value={target}
+                  onChange={(e) => setTarget(e.target.value)}
+                  onKeyDown={(e) => {
+                    if (e.code === 'Enter') {
+                      editTarget()
+                    } else if (e.code === 'Escape') {
+                      setTarget(data.target_value)
+                      setAllowEditTarget(false)
+                    }
+                  }}
+                />
+                <Button type='button' variant={'ghost'} onClick={editTarget}>
+                  Edit
+                </Button>
+              </div>
+            ) : (
+              <p title='Click to edit' onClick={() => setAllowEditTarget(true)}>
+                {formatIndianMoneyNotation(data.target_value)}
+              </p>
+            )}
+          </div>
         </div>
 
         <div className='flex'>
@@ -70,7 +192,26 @@ export default function GoalDetails() {
             <GaugeIcon className='w-4 h-4' />
             <div>KPI</div>
           </div>
-          <div className='flex-1 font-medium'>{data.track_kpi}</div>
+          <div className='flex-1 font-medium'>
+            <Select
+              value={kpi}
+              onValueChange={(data) => {
+                setKpi(data)
+                goalMutation.mutate({ track_kpi: data })
+              }}
+            >
+              <SelectTrigger>
+                <SelectValue>{kpi}</SelectValue>
+              </SelectTrigger>
+              <SelectContent>
+                {getTargetKpi?.map((item) => (
+                  <SelectItem key={item.id} value={item.value}>
+                    {item.label}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </div>
         </div>
 
         <div className='flex'>
@@ -78,7 +219,25 @@ export default function GoalDetails() {
             <SplitIcon className='w-4 h-4' />
             <div>Source</div>
           </div>
-          <div className='flex-1 font-medium'>{data.target_contribution_source}</div>
+          <div className='flex-1 font-medium'>
+            <Select
+              value={source}
+              onValueChange={(value) => {
+                goalMutation.mutate({ target_contribution_source: data ? getTargetId(value) : undefined })
+              }}
+            >
+              <SelectTrigger>
+                <SelectValue placeholder='Select a source'>{source}</SelectValue>
+              </SelectTrigger>
+              <SelectContent placeholder='Select a source'>
+                {incomeExpenses?.map((item) => (
+                  <SelectItem key={item.id} value={item.name}>
+                    {item.name}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </div>
         </div>
 
         <div className='flex'>
@@ -88,8 +247,31 @@ export default function GoalDetails() {
               <p>{data?.reachable_by_days < 0 ? 'Reached' : 'Reachable by'}</p>
             </div>
           </div>
-          <div className='flex-1 font-medium'>
-            <p>{reachableDays}</p>
+          <div className='flex-1 font-medium self-center'>
+            {allowEditReachableDate ? (
+              <div className='flex gap-4 w-72'>
+                <Input
+                  type='number'
+                  value={reachableDate}
+                  onChange={(e) => setRechableDate(e.target.value)}
+                  onKeyDown={(e) => {
+                    if (e.code === 'Enter') {
+                      editReachableDays()
+                    } else if (e.code === 'Escape') {
+                      setRechableDate(data?.reachable_by_days)
+                      setAllowEditReachableDate(false)
+                    }
+                  }}
+                />
+                <Button type='button' variant={'ghost'} onClick={editReachableDays}>
+                  Edit
+                </Button>
+              </div>
+            ) : (
+              <p title='Click to edit' onClick={() => setAllowEditReachableDate(true)}>
+                {reachableDaysString}
+              </p>
+            )}
           </div>
         </div>
       </div>

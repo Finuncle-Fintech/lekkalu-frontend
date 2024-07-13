1043323aaaaba739498eca44cf2a21274f0f6a3f
Bharath Kumar Chowdary Gundala <bharathkumarchowdary006@gmail.com>
2024-07-14 01:33:56 +0530

    changes small code

--- src/pages/KPIs/CreateCustomKPI.tsx
+++ src/pages/KPIs/CreateCustomKPI.tsx
@@ -2,15 +2,16 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { ArrowLeftIcon } from 'lucide-react'
 import CustomKPIFlow from '@/pages/KPIs/Components/CustomKPIFlow'
+import Page from '@/components/Page/Page'
 
 export default function CreateCustomKPI() {
   return (
-    <div>
+    <Page>
       <Link className="flex items-center ml-10 mt-4 gap-2 text-muted-foreground" to="/kpis">
         <ArrowLeftIcon className="w-4 h-4" />
         Back to KPIs
       </Link>
       <CustomKPIFlow />
-    </div>
+    </Page>
   )
 }

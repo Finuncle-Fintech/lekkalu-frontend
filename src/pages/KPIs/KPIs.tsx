05f70983cee242e46242d9494233961c6fd01992
Bharath Kumar Chowdary Gundala <bharathkumarchowdary006@gmail.com>
2024-07-13 23:54:01 +0530

    further refines KPIs page

--- src/pages/KPIs/KPIs.tsx
+++ src/pages/KPIs/KPIs.tsx
@@ -9,32 +9,42 @@ export default function Goals() {
   const { data, isLoading, isFetching } = useQuery({ queryKey: [KPIS.KPIS], queryFn: fetchCustomKPIs })
   return (
     <div>
-      <div className="text-4xl font-bold text-center mt-8 mb-4">Key Performance Indicators</div>
-      <div className="text-sm text-center mt-4 mb-2">
-        Create your own KPIs visually to track them later on your financial goals or to analyze outcomes of financial
-        decisions
-      </div>
-      {isLoading && <p>Loading...</p>}
-      {isFetching && <p>Fetching...</p>}
-      <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-10">
-        {data?.length ? (
-          data?.map((custom_kpi) => (
-            <CustomKPICard
-              key={custom_kpi.id}
-              id={custom_kpi.id}
-              name={custom_kpi.name}
-              description={custom_kpi.description}
-            />
-          ))
-        ) : (
-          <div>
-            <p>You Have no financial goals.</p>
-            <Link to="/goals/new" className="block underline mt-2">
-              Click here to add.
-            </Link>{' '}
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-wrap w-full mb-20 flex-col items-center text-center">
+            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Key Performance
+              Indicators</h1>
+            <p className="lg:w-1/2 w-full leading-relaxed text-gray-500">Create your own KPIs visually to track them
+              later on your financial goals or to analyze outcomes of financial
+              decisions.</p>
           </div>
-        )}
-      </div>
+          <button
+            className="flex mx-auto mt-4 mb-4 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Add
+            KPI
+          </button>
+          {isLoading && <p>Loading...</p>}
+          {isFetching && <p>Fetching...</p>}
+          <div className="flex flex-wrap -m-4">
+            {data?.length ? (
+              data?.map((custom_kpi) => (
+                <CustomKPICard
+                  key={custom_kpi.id}
+                  id={custom_kpi.id}
+                  name={custom_kpi.name}
+                  description={custom_kpi.description}
+                />
+              ))
+            ) : (
+              <div>
+                <p>You Have no financial goals.</p>
+                <Link to="/goals/new" className="block underline mt-2">
+                  Click here to add.
+                </Link>{' '}
+              </div>
+            )}
+          </div>
+        </div>
+      </section>
     </div>
   )
 }

9b85845648129f4ed2beb99f4d9fb4e8d04d0bcf
Sandip Satyal <sandipsatyal25@gmail.com>
2024-09-27 18:50:02 +0545

    upgrade: react-day-picker to v9

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -13,44 +13,48 @@ function Calendar({ className, classNames, showOutsideDays = true, ...props }: C
     <DayPicker
       showOutsideDays={showOutsideDays}
       className={cn('p-3', className)}
+      captionLayout='dropdown'
+      mode='single'
       classNames={{
         months: 'flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0',
         month: 'space-y-4',
-        caption: 'flex justify-center pt-1 relative items-center',
+        month_caption: 'flex justify-center pt-1 relative items-center',
         caption_label: props.mode === 'range' ? 'block' : 'hidden',
-        caption_dropdowns: 'flex justify-center gap-1',
+        dropdowns: 'flex justify-center gap-1',
         nav: 'space-x-1 flex items-center',
-        nav_button: cn(
+        button_previous: cn(
           buttonVariants({ variant: 'outline' }),
-          'h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100',
+          'h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100 absolute left-1',
         ),
-        nav_button_previous: 'absolute left-1',
-        nav_button_next: 'absolute right-1',
-        table: 'w-full border-collapse space-y-1',
-        head_row: 'flex',
-        head_cell: 'text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]',
-        row: 'flex w-full mt-2',
-        cell: 'text-center text-sm p-0 relative [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20',
-        day: cn(buttonVariants({ variant: 'ghost' }), 'h-9 w-9 p-0 font-normal aria-selected:opacity-100'),
-        day_selected:
-          'bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground',
-        day_today: 'bg-accent text-accent-foreground',
-        day_outside: 'text-muted-foreground opacity-50',
-        day_disabled: 'text-muted-foreground opacity-50',
-        day_range_middle: 'aria-selected:bg-accent aria-selected:text-accent-foreground',
-        day_hidden: 'invisible',
+        button_next: cn(
+          buttonVariants({ variant: 'outline' }),
+          'h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100 absolute right-1',
+        ),
+        month_grid: 'w-full border-collapse space-y-1 mr-5',
+        weekdays: 'flex',
+        weekday: 'text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]',
+        week: 'flex w-full mt-2',
+        day: 'text-center text-sm p-0 relative [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20',
+        day_button: cn(buttonVariants({ variant: 'ghost' }), 'h-9 w-9 p-0 font-normal aria-selected:opacity-100'),
+        selected:
+          'bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground rounded-lg',
+        today: 'bg-accent text-accent-foreground rounded-lg',
+        outside: 'text-muted-foreground opacity-50',
+        disabled: 'text-muted-foreground opacity-50',
+        range_middle: 'aria-selected:bg-accent aria-selected:text-accent-foreground',
+        hidden: 'invisible',
         ...classNames,
       }}
       components={{
-        Dropdown: ({ value, onChange, children }: DropdownProps) => {
-          const options = React.Children.toArray(children) as React.ReactElement<React.HTMLProps<HTMLOptionElement>>[]
-          const selected = options.find((child) => child.props.value === value)
+        Dropdown: ({ value, onChange, options }: DropdownProps) => {
           const handleChange = (value: string) => {
             const changeEvent = {
               target: { value },
             } as React.ChangeEvent<HTMLSelectElement>
             onChange?.(changeEvent)
           }
+          const current_label = options?.findIndex((each) => each.value === value) as number
+          const _selectedValue = options?.length === 12 ? options[current_label]?.label : value
           return (
             <Select
               value={value?.toString()}
@@ -58,23 +62,29 @@ function Calendar({ className, classNames, showOutsideDays = true, ...props }: C
                 handleChange(value)
               }}
             >
-              <SelectTrigger className='pr-1.5 focus:ring-0'>
-                <SelectValue>{selected?.props?.children}</SelectValue>
+              <SelectTrigger className='pr-1.5 focus:ring-0' value={value}>
+                <SelectValue>{_selectedValue}</SelectValue>
               </SelectTrigger>
               <SelectContent position='popper'>
                 <ScrollArea className='h-80'>
-                  {options.map((option, id: number) => (
-                    <SelectItem key={`${option.props.value}-${id}`} value={option.props.value?.toString() ?? ''}>
-                      {option.props.children}
-                    </SelectItem>
-                  ))}
+                  {options?.map((option, id: number) => {
+                    return (
+                      <SelectItem key={`${option.value}-${id}`} value={option.value?.toString() ?? ''}>
+                        {option.label}
+                      </SelectItem>
+                    )
+                  })}
                 </ScrollArea>
               </SelectContent>
             </Select>
           )
         },
-        IconLeft: ({ ...props }) => <ChevronLeft className='h-4 w-4' {...props} />,
-        IconRight: ({ ...props }) => <ChevronRight className='h-4 w-4' {...props} />,
+        Chevron: (props) => {
+          if (props.orientation === 'left') {
+            return <ChevronLeft className='h-4 w-4' {...props} />
+          }
+          return <ChevronRight className='h-4 w-4' {...props} />
+        },
       }}
       {...props}
     />

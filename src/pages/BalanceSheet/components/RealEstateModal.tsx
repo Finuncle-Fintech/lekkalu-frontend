af487cd99b60ceb582fdc984d5851c78dd9196f8
Bharath Kumar Chowdary <bharathkumarchowdary006@gmail.com>
2024-10-19 22:20:53 +0530

    abstracts AssetModal.tsx into a separate component to reuse for all asset types removes unnecessary field field_name for DetailFieldProps

--- src/pages/BalanceSheet/components/RealEstateModal.tsx
+++ src/pages/BalanceSheet/components/RealEstateModal.tsx
@@ -1,16 +1,10 @@
-import React, { useEffect, useState } from 'react'
-import { ChevronDown, House, IndianRupee, Percent } from 'lucide-react'
-import { zodResolver } from '@hookform/resolvers/zod'
+import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
-import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { IndianRupee, Percent } from 'lucide-react'
+import AssetModal, { FieldProp } from '@/pages/BalanceSheet/components/AssetModal'
+import { addPhysicalAssetSchemaV1, AddPhysicalAssetSchemaV1 } from '@/schema/balance-sheet'
 import DatePickerV1 from '@/components/DatePicker/DatePickerV1'
-import GenericFormField, { CancelButton, DetailField, Field, FieldContainer, SaveButton } from '@/components/ui/form-v1'
-import logger from '@/logger'
-import { AddPhysicalAssetSchemaV1, addPhysicalAssetSchemaV1 } from '@/schema/balance-sheet'
-import { addPhysicalAssetV1 } from '@/queries/balance-sheet'
-import { BALANCE_SHEET } from '@/utils/query-keys'
-import { getErrorMessage } from '@/utils/utils'
-import { toast } from '@/components/ui/use-toast'
 
 type AssetModalProps = {
   isDialogOpen: boolean
@@ -20,167 +14,63 @@ type AssetModalProps = {
     payload: 'Real Estate' | 'Metal' | 'Equity' | 'Bank Account' | 'Mutual Fund'
   }>
 }
+
 export default function RealEstateModal({ isDialogOpen, setIsDialogOpen, dispatch }: AssetModalProps) {
-  const form = useForm<AddPhysicalAssetSchemaV1>({
+  const [selectedDate, setSelectedDate] = useState<Date | undefined>(undefined)
+  const assetForm = useForm<AddPhysicalAssetSchemaV1>({
     resolver: zodResolver(addPhysicalAssetSchemaV1),
   })
-  const qc = useQueryClient()
-  const addPhysicalAssetMutation = useMutation({
-    mutationFn: addPhysicalAssetV1,
-    onSuccess: () => {
-      qc.invalidateQueries({ queryKey: [BALANCE_SHEET.ASSETS] })
-      toast({ title: 'Asset created successfully!' })
-      setIsDialogOpen(false)
-    },
-    onError: (err) => toast(getErrorMessage(err)),
-  })
-  const handleAddOrEditPhysicalAsset = (data: AddPhysicalAssetSchemaV1) => {
-    logger.info(data)
-    addPhysicalAssetMutation.mutate(data)
-  }
-  const onCancel = () => {
-    setIsDialogOpen(false)
-  }
-  const [selectedDate, setSelectedDate] = useState<Date | undefined>(undefined)
-  const [isOverlayVisible, setIsOverlayVisible] = useState(false)
+  const fields: Array<Array<FieldProp>> = [
+    [
+      {
+        name: 'purchase_date',
+        label: 'Bought On',
+        placeholder: '40,00,000',
+        type: 'number',
+        error: assetForm.formState.errors.purchase_date,
+        icon: (
+          <DatePickerV1
+            value={selectedDate}
+            onChange={(date) => {
+              setSelectedDate(date)
+              if (date) {
+                assetForm.setValue('purchase_date', date)
+              }
+            }}
+            className='mr-2 h-4 w-4'
+          />
+        ),
+      },
+      {
+        name: 'purchase_value',
+        label: 'At Price',
+        placeholder: 'Sun, 14 Jul',
+        type: 'number',
+        error: assetForm.formState.errors.purchase_date,
+        icon: <IndianRupee className='mr-2 h-4 w-4' />,
+      },
+    ],
+    [
+      {
+        name: 'expected_returns',
+        label: 'Expected Returns',
+        placeholder: '4',
+        type: 'number',
+        error: assetForm.formState.errors.expected_returns,
+        icon: <Percent />,
+      },
+    ],
+  ]
 
-  const toggleOverlay = () => {
-    setIsOverlayVisible(!isOverlayVisible)
-  }
-
-  useEffect(() => {
-    form.clearErrors()
-  }, [form, isDialogOpen])
   return (
-    <form onSubmit={form.handleSubmit(handleAddOrEditPhysicalAsset)}>
-      <div className='h-auto p-[25px] bg-accent rounded-[10px] shadow justify-start items-start gap-[31px] inline-flex'>
-        <div className='flex-col justify-center items-start gap-2.5 inline-flex'>
-          <div className="text-right text-black text-2xl font-bold font-['Charter'] leading-[33.60px]">About</div>
-          <div className="w-[270px] h-[150px] text-black text-[15px] font-normal font-['Charter'] leading-[21px]">
-            Add any Real Estate properties that you own. Flat in an apartment, Commercial shop. And specify expected
-            returns to gauge your total financial performance over past and future
-          </div>
-        </div>
-        <div className='flex-col justify-start items-start gap-[31px] inline-flex'>
-          <div
-            onClick={toggleOverlay}
-            className='px-2.5 py-[5px] bg-[#c6ddff] rounded-[5px] justify-center items-center gap-2.5 inline-flex cursor-pointer'
-          >
-            <div className='w-[15px] h-[15px] relative'>
-              <House className='w-[15px] h-[15px]' />
-            </div>
-            <div className="text-black text-base font-normal font-['Charter'] leading-snug">Real Estate</div>
-            <div className='w-[15px] h-[15px] p-[1.25px] justify-center items-center flex'>
-              <ChevronDown className='w-[15px] h-[15px]' />
-            </div>
-          </div>
-          {isOverlayVisible && (
-            <div className='absolute bg-white shadow-md rounded-md mt-2'>
-              <ul>
-                <li
-                  className='p-2 cursor-pointer hover:bg-gray-200'
-                  onClick={() => dispatch({ type: 'SET_MODAL', payload: 'Real Estate' })}
-                >
-                  Option 1
-                </li>
-                <li
-                  className='p-2 cursor-pointer hover:bg-gray-200'
-                  onClick={() => dispatch({ type: 'SET_MODAL', payload: 'Metal' })}
-                >
-                  Option 2
-                </li>
-                <li
-                  className='p-2 cursor-pointer hover:bg-gray-200'
-                  onClick={() => dispatch({ type: 'SET_MODAL', payload: 'Equity' })}
-                >
-                  Option 3
-                </li>
-              </ul>
-            </div>
-          )}
-          <Field error={form.formState.errors.name}>
-            <GenericFormField
-              name={'name'}
-              register={form.register}
-              placeholder={'Asset Name'}
-              className="w-full bg-inherit self-stretch text-black/25 text-4xl font-normal font-['Charter'] leading-9
-              focus:outline-none"
-            />
-          </Field>
-          {/*Container of Essential Fields*/}
-          <FieldContainer>
-            {/*Bought Date Field*/}
-            <DetailField label='Bought on' error={form.formState.errors.purchase_date}>
-              <div className='justify-start items-center gap-[5px]  inline-flex'>
-                <GenericFormField
-                  type='text'
-                  placeholder='Sun, 14 Jul'
-                  register={form.register}
-                  name='purchase_date'
-                  className="bg-inherit text-black text-base font-normal font-['Charter'] leading-snug
-                      whitespace-nowrap"
-                  disabled={true}
-                  textAlign='right'
-                  value={selectedDate ? selectedDate.toISOString().split('T')[0] : undefined}
-                />
-                <DatePickerV1
-                  value={selectedDate}
-                  onChange={(date) => {
-                    setSelectedDate(date)
-                    if (date) {
-                      form.setValue('purchase_date', date)
-                    }
-                  }}
-                  className='mr-2 h-4 w-4'
-                />
-              </div>
-            </DetailField>
-            {/*Bought Price Field*/}
-            <DetailField label='At Price' error={form.formState.errors.purchase_value} field_name='buy_price'>
-              <div className='justify-start items-center gap-[5px] inline-flex'>
-                <GenericFormField
-                  type='number'
-                  placeholder='40,00,000'
-                  register={form.register}
-                  valueAsNumber={true}
-                  name='purchase_value'
-                  className="bg-inherit text-black font-normal font-['Charter'] leading-snug focus:outline-none
-                      text-right min-w-10 max-w-20"
-                />
-                <IndianRupee className='mr-2 h-4 w-4' />
-              </div>
-            </DetailField>
-          </FieldContainer>
-          <FieldContainer>
-            <DetailField
-              label='Expected Returns'
-              error={form.formState.errors.expected_returns}
-              field_name={'expected_returns'}
-            >
-              <div className='justify-start items-center gap-[5px] inline-flex'>
-                <GenericFormField
-                  type='number'
-                  name='expected_returns'
-                  register={form.register}
-                  valueAsNumber={true}
-                  placeholder='4'
-                  className="bg-inherit text-black font-normal font-[' Charter'] leading-snug focus:outline-none
-                      min-w-10 max-w-20"
-                  textAlign='right'
-                />
-                <Percent className='mr-2 h-4 w-4' />
-              </div>
-            </DetailField>
-          </FieldContainer>
-          <div className='self-stretch justify-between items-start inline-flex'>
-            <CancelButton clickHandler={onCancel} />
-            <SaveButton />
-          </div>
-        </div>
-      </div>
-      <input type='hidden' {...form.register('type')} value='1' />
-    </form>
+    <AssetModal
+      isDialogOpen={isDialogOpen}
+      setIsDialogOpen={setIsDialogOpen}
+      description='Add any Real Estate properties that you own. Flat in an apartment, Commercial shop.
+      And specify expected returns to gauge your total financial performance over past and future '
+      fields={fields}
+      dispatch={dispatch}
+      assetForm={assetForm}
+    />
   )
 }
-
-// todo: fix the date picker
